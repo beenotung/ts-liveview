a5164c52b15c0289dcac07dbb86910b76ac5b905
Beeno Tung <aabbcc1241@yahoo.com.hk>
2020-04-12 12:31:04 +0800

    fix missing dep

--- app/lib.ts
+++ app/lib.ts
@@ -1,8 +1,1 @@
-export { DataSignal } from 's-js'
-export { SArray, SDataArray, SSignalArray } from 's-array'
-import S from 's-js'
-
-export const data = S.data
-export const value = S.value
-
 export * from '../src'
