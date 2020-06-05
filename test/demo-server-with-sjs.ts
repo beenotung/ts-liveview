b43a4ece39e9864fed5ef65b4bbd15e95cde1adc
Beeno Tung <aabbcc1241@yahoo.com.hk>
2020-06-05 18:20:26 +0800

    remove redundant S.root and dispose in sjs demo in test and readme

--- test/demo-server-with-sjs.ts
+++ test/demo-server-with-sjs.ts
@@ -26,61 +26,59 @@ function initialView(req: Request, res: Response) {
   )
 }
 
+// this callback will be called from a S.root context
+// the context will be cleanup automatically when the client connection is closed
 function createSession(session: Session): Session | void {
-  S.root(dispose => {
-    session.attachDispose(dispose)
+  const clock = S.data(Date.now())
+  const timer = setInterval(() => clock(Date.now()), 1000)
+  S.cleanup(() => clearInterval(timer))
+  setInterval(() => clock(Date.now()), 1000)
 
-    const clock = S.data(Date.now())
-    const timer = setInterval(() => clock(Date.now()), 1000)
-    S.cleanup(() => clearInterval(timer))
-    setInterval(() => clock(Date.now()), 1000)
+  function renderClock() {
+    return c(
+      '#clock',
+      h`<p id="clock">Now is: ${new Date(clock()).toLocaleString()}</p>`,
+    )
+  }
 
-    function renderClock() {
-      return c(
-        '#clock',
-        h`<p id="clock">Now is: ${new Date(clock()).toLocaleString()}</p>`,
-      )
-    }
+  const name = S.data('')
 
-    const name = S.data('')
-
-    function renderName() {
-      return c(
-        '#name',
-        h`<div id="name">
+  function renderName() {
+    return c(
+      '#name',
+      h`<div id="name">
 <label>Name: </label>
 <input onchange="send('name', event.target.value)">
 <p>
 Hello, ${name() || 'Guest'}
 </p>
 </div>`,
-      )
-    }
+    )
+  }
 
-    function renderRoot() {
-      return S.sample(() =>
-        c(
-          '#app',
-          h`<div id="app" class="live">
+  function renderRoot() {
+    return S.sample(() =>
+      c(
+        '#app',
+        h`<div id="app" class="live">
 ${renderClock()}
 ${renderName()}
 </div>`,
-        ),
-      )
+      ),
+    )
+  }
+
+  session.sendComponent(renderRoot())
+  session.live(renderClock, { skipInitialSend: true })
+  session.live(renderName, { skipInitialSend: true })
+
+  session.onMessage(message => {
+    const [k, v] = message
+    if (k !== 'name') {
+      console.warn('unknown client message:', message)
+      return
     }
-
-    session.sendComponent(renderRoot())
-    session.live(renderClock, { skipInitialSend: true })
-    session.live(renderName, { skipInitialSend: true })
-
-    session.onMessage(message => {
-      const [k, v] = message
-      if (k !== 'name') {
-        console.warn('unknown client message:', message)
-        return
-      }
-      name(v)
-    })
+    name(v)
   })
 
   return session
