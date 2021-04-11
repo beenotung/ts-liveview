82bf29817175291738077c80c26946e44eb1093f
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-04-11 10:14:45 +0800

    minify inline css in prod mode

--- server/components.tsx
+++ server/components.tsx
@@ -2,7 +2,16 @@ import JSX from '../client/jsx.js'
 import { Raw } from './dom.js'
 
 export function Style(props: { css: string }) {
-  let css = props.css.trim()
+  let css = props.css
+  if (process.env.NODE_ENV === 'prod') {
+    css = css
+      .split('\n')
+      .map(line => line.trim())
+      .filter(line => line)
+      .join('\n')
+  } else {
+    css = css.trim()
+  }
   return (
     <style>
       <Raw html={css} />
