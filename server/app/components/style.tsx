49f4a8d0444b479f04bac6e0faf26041713594d4
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-07-10 14:08:55 +0800

    fix import for css minifier

--- server/app/components/style.tsx
+++ server/app/components/style.tsx
@@ -1,6 +1,6 @@
 import { Raw } from './raw.js'
 import JSX from '../jsx/jsx.js'
-import * as minify from 'minify'
+import minify from 'minify'
 
 export function Style(css: string) {
   if (process.env.NODE_ENV === 'production') {
