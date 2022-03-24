c526062d2ead5f9841852267f0843239372f596e
Beeno Tung <aabbcc1241@yahoo.com.hk>
2022-03-24 08:55:26 +0800

    add charset in response type

--- server/app/express.ts
+++ server/app/express.ts
@@ -2,7 +2,7 @@ import express from 'express'
 
 export function sendHTML(res: express.Response, html: string) {
   res.setHeader('Connection', 'Keep-Alive')
-  res.setHeader('Content-Type', 'text/html')
+  res.setHeader('Content-Type', 'text/html; charset=UTF-8')
   // res.setHeader('Link', `<http://localhost:8100/${page}>; rel="canonical"`)
   res.end(html)
 }
