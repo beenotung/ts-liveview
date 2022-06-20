4d48051dcc43762e914228579315b5470bdb364a
Beeno Tung <aabbcc1241@yahoo.com.hk>
2022-06-21 07:29:30 +0800

    add text.tsx to auto inject br on text

---
+++ server/app/components/text.tsx
@@ -0,0 +1,12 @@
+import JSX from '../jsx/jsx.js'
+import type { Fragment, Node } from '../jsx/types'
+
+/** @description consider set "white-space" css property instead */
+function text(text: string): Fragment {
+  let nodes: Node[] = []
+  text.split('\n').forEach(line => nodes.push(line, <br />))
+  nodes.pop()
+  return [nodes]
+}
+
+export default text
