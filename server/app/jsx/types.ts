1d3abf2703138066bf7bd1cd2019171bcb34483d
Beeno Tung <aabbcc1241@yahoo.com.hk>
2022-06-26 14:54:36 +0800

    make component function generic

--- server/app/jsx/types.ts
+++ server/app/jsx/types.ts
@@ -2,19 +2,18 @@ import { Context } from '../context'
 
 export type Node = Literal | Raw | Fragment | JSXFragment | Component | Element
 export type JSXFragment = [tag: undefined, attr: null, children: NodeList]
-export type Component = [ComponentFn, attrs?, NodeList?]
+export type Component<Attrs = CFAttrs> = [ComponentFn<Attrs>, attrs?, NodeList?]
 export type Element = [selector, attrs?, NodeList?]
 export type NodeList = Node[]
 export type Literal = string | number | null | undefined | false
 export type Raw = ['raw', html]
 export type Fragment = [NodeList]
 
-/** @alias FC */
-export type ComponentFn = (
-  attrs: {
-    children?: NodeList
-    [key: string]: unknown
-  },
+type CFAttrs = { children?: NodeList }
+
+/** similar to React.FC */
+export type ComponentFn<Attrs extends CFAttrs> = (
+  attrs: Attrs,
   context: Context,
 ) => Node
 
