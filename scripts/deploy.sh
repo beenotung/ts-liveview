069e9b66950553fe7dc72a39a2addaa6bfafab63
Beeno Tung <aabbcc1241@yahoo.com.hk>
2022-06-29 02:51:36 +0800

    restore quick version of deploy flow when MODE flag

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,16 @@ set -o pipefail
 
 source scripts/config
 npm run build
+if [ "$MODE" == "quick" ]; then
+rsync -SavlP \
+  build \
+	dist \
+	"$user@$host:$root_dir"
+ssh "$user@$host" " \
+source ~/.nvm/nvm.sh \
+&& pm2 reload $pm2_name \
+"
+else
 rsync -SavlP \
   build \
 	dist \
@@ -19,3 +29,4 @@ source ~/.nvm/nvm.sh \
 && npm run migrate \
 && pm2 reload $pm2_name \
 "
+fi
