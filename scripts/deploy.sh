0689125e2722f5b993c3b3e7cfd1a032f0722e0f
Beeno Tung <aabbcc1241@yahoo.com.hk>
2022-06-29 02:46:50 +0800

    include package install and db migrate in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,5 +7,14 @@ npm run build
 rsync -SavlP \
   build \
 	dist \
+	package.json \
 	"$user@$host:$root_dir"
-ssh "$user@$host" "source ~/.nvm/nvm.sh && pm2 reload $pm2_name"
+ssh "$user@$host" " \
+source ~/.nvm/nvm.sh \
+&& set -x \\
+&& cd $root_dir \
+&& pnpm i -r \
+&& cd db \
+&& npm run migrate \
+&& pm2 reload $pm2_name \
+"
