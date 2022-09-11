4c3a1826847255426e3f2866fbd0431be309a122
Beeno Tung <aabbcc1241@yahoo.com.hk>
2022-09-11 22:47:09 +0800

    update proxy.ts with erd-to-proxy generator in quick-erd@3.3.1

--- db/proxy.ts
+++ db/proxy.ts
@@ -1,39 +1,50 @@
 import { proxySchema } from 'better-sqlite3-proxy'
 import { db } from './db.js'
 
-export type RequestLog = {
-  id?: number
-  method_id: number
-  url_id: number
-  user_agent_id: number | null
-  timestamp: number
-}
-
 export type Method = {
-  id?: number
+  id?: number | null
   method: string
 }
 
 export type Url = {
-  id?: number
+  id?: number | null
   url: string
 }
 
 export type UserAgent = {
-  id?: number
+  id?: number | null
   user_agent: string
 }
 
+export type RequestLog = {
+  id?: number | null
+  method_id: number
+  method?: Method
+  url_id: number
+  url?: Url
+  user_agent_id: number | null
+  user_agent?: UserAgent
+  timestamp: number
+}
+
 export type DBProxy = {
-  request_log: RequestLog[]
   method: Method[]
   url: Url[]
   user_agent: UserAgent[]
+  request_log: RequestLog[]
 }
 
-export let proxy = proxySchema<DBProxy>(db, {
-  request_log: [],
-  method: [],
-  url: [],
-  user_agent: [],
+export let proxy = proxySchema<DBProxy>({
+  db,
+  tableFields: {
+    method: [],
+    url: [],
+    user_agent: [],
+    request_log: [
+      /* foreign references */
+      ['method', { field: 'method_id', table: 'method' }],
+      ['url', { field: 'url_id', table: 'url' }],
+      ['user_agent', { field: 'user_agent_id', table: 'user_agent' }],
+    ],
+  },
 })
