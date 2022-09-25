5f6c2d24ff7603438846fc87af30b0c023a33e1d
Beeno Tung <aabbcc1241@yahoo.com.hk>
2022-09-26 02:05:23 +0800

    move error style close to the render function

--- server/app/components/error.ts
+++ server/app/components/error.ts
@@ -1,6 +1,14 @@
 import type { VNode } from '../../../client/jsx/types'
 import type { Context } from '../context'
 
+export let ErrorStyle = /* css */ `
+.error {
+  border: 1px solid red;
+  padding: 0.75rem;
+  width: fit-content;
+}
+`
+
 export function renderError(error: unknown, context: Context): VNode {
   if (context.type === 'express' && !context.res.headersSent && error) {
     let code = (error as any).statusCode || (error as any).status || 500
