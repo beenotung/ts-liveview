03e77d4d89f5971d8bb1f074094ed83d281282b2
Beeno Tung <aabbcc1241@yahoo.com.hk>
2022-10-04 14:40:34 +0800

    rename db filename from dev.sqlite3 to db.sqlite3

--- db/db.ts
+++ db/db.ts
@@ -1,7 +1,7 @@
 import { newDB } from 'better-sqlite3-schema'
 import { join } from 'path'
 
-export let dbFile = join('data', 'dev.sqlite3')
+export let dbFile = join('data', 'db.sqlite3')
 
 export let db = newDB({
   path: dbFile,
