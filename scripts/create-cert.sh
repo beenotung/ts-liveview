040ab127d18453d2c5bf45781da924bc53ac1a6e
Beeno Tung <aabbcc1241@yahoo.com.hk>
2022-12-31 18:56:01 +0800

    make deps on mkcert optional
    skip calling it if it's not installed

--- scripts/create-cert.sh
+++ scripts/create-cert.sh
@@ -3,5 +3,12 @@
 set -e
 set -o pipefail
 
+function skipcert {
+	echo "skipping self-signed https cert since mkcert is not installed"
+	[ ! -f .env ] && cp .env .env.example
+	sed -i '/HTTP_VERSION/d' .env
+	echo "HTTP_VERSION=1" >.env
+}
+
 # openssl req -nodes -new -x509 -keyout server.key -out server.cert
-mkcert -install && mkcert localhost
+mkcert -install && mkcert localhost || skipcert
