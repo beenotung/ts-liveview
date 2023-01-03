1331ae65f99f5672cc0e4685c30e465e62ef46ba
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-01-04 02:53:32 +0800

    enable http2 after self-signed cert is generated successfully

--- scripts/create-cert.sh
+++ scripts/create-cert.sh
@@ -3,12 +3,21 @@
 set -e
 set -o pipefail
 
+[ ! -f .env ] && cp .env.example .env
+
+sed -i.bk '/HTTP_VERSION/d' .env
+
+function usecert {
+	echo "enabling http2 with self-signed https cert"
+	echo "HTTP_VERSION=2" >>.env
+}
+
 function skipcert {
 	echo "skipping self-signed https cert since mkcert is not installed"
-	[ ! -f .env ] && cp .env .env.example
-	sed -i '/HTTP_VERSION/d' .env
-	echo "HTTP_VERSION=1" >.env
+	echo "HTTP_VERSION=1" >>.env
 }
 
 # openssl req -nodes -new -x509 -keyout server.key -out server.cert
-mkcert -install && mkcert localhost || skipcert
+mkcert -install && mkcert localhost && usecert || skipcert
+
+rm .env.bk
