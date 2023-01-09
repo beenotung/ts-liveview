0aed95df7ed072e47d0dd98c32fc5634911eb4e2
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-01-10 01:53:55 +0800

    shorten top menu text for 404 demo page

--- server/app/components/top-menu.tsx
+++ server/app/components/top-menu.tsx
@@ -8,6 +8,6 @@ let routes: MenuRoutes = menuRoutes.map(route => [
   route.url,
 ])
 
-routes.push(['/some/page/that/does-not/exist', 'some page that does not exist'])
+routes.push(['/some/page/that/does-not/exist', '404'])
 
 export let topMenu = <Menu attrs={{ style: 'margin: 1em 0' }} routes={routes} />
