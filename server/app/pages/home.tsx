99c983ce131a38aa062ada74e121b082ad273d66
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-01-29 07:16:37 +0800

    chore: remove demo pages from minimal-template chore: hide noscript message by default chore: simplify home page

--- server/app/pages/home.tsx
+++ server/app/pages/home.tsx
@@ -28,48 +28,6 @@ let content = (
         zh_cn="你可以从修改此页的内容开始。"
       />
     </p>
-
-    <p>
-      <Locale
-        en={
-          <>
-            When the browser loads this URL, the server responds with complete
-            HTML content to the GET request. This allows the browser to perform
-            meaningful paint as soon as possible, which is ideal for SEO.
-          </>
-        }
-        zh_hk={
-          <>
-            當瀏覽器載入此網址時，伺服器會回應完整的 HTML 內容給 GET
-            請求，讓瀏覽器能夠盡快完成有意義的頁面渲染 (meaningful paint)，這對
-            SEO（搜尋引擎優化）是非常理想的。
-          </>
-        }
-        zh_cn={
-          <>
-            当浏览器载入此网址时，服务器会响应完整的 HTML 内容给 GET
-            请求，让浏览器能够尽快完成有意义的页面渲染（meaningful paint），这对
-            SEO（搜索引擎优化）是非常理想的。
-          </>
-        }
-      />
-    </p>
-
-    <p>
-      <Locale
-        en="Try some reactive demos:"
-        zh_hk="試試一些反應式 (reactive) 範例:"
-        zh_cn="试试一些反应式 (reactive) 示例:"
-      />{' '}
-      <a href="https://liveviews.cc/thermostat" target="_blank">
-        <Locale en="Thermostat" zh_hk="溫控器" zh_cn="温控器" />
-      </a>
-      ,{' '}
-      <a href="https://liveviews.cc/form" target="_blank">
-        <Locale en="Form Demo" zh_hk="表單範例" zh_cn="表单示例" />
-      </a>
-    </p>
-
     <SourceCode page="home.tsx" />
   </div>
 )
