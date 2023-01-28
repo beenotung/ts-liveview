0d2e43ae598ebdb1a06fdfb39e0aa2d26028036b
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-01-29 07:16:37 +0800

    chore: remove demo pages from minimal-template chore: hide noscript message by default chore: simplify home page

--- server/app/app.tsx
+++ server/app/app.tsx
@@ -14,7 +14,6 @@ import { OnWsMessage } from '../ws/wss.js'
 import { dispatchUpdate } from './jsx/dispatch.js'
 import { EarlyTerminate } from './helpers.js'
 import { getWSSession } from './session.js'
-import DemoCookieSession from './pages/demo-cookie-session.js'
 import { Flush } from './components/flush.js'
 import { config } from '../config.js'
 import Stats from './stats.js'
@@ -25,13 +24,11 @@ import {
   PageRouteMatch,
   redirectDict,
 } from './routes.js'
-import Chatroom from './pages/chatroom.js'
 import type { ClientMountMessage, ClientRouteMessage } from '../../client/types'
 import { then } from '@beenotung/tslib/result.js'
 import { appStyle } from './app-style.js'
 import { renderIndexTemplate } from '../../template/index.js'
 import { HTMLStream } from './jsx/stream.js'
-import DemoUpload from './pages/demo-upload.js'
 import { getWsCookies } from './cookie.js'
 import { PickLanguage } from './components/ui-language.js'
 import Navbar from './components/navbar.js'
@@ -156,9 +153,6 @@ function Footer(attrs: { style?: string }) {
 // prefer flat router over nested router for less overhead
 export function attachRoutes(app: Router) {
   // ajax/middleware routes
-  DemoCookieSession.attachRoutes(app)
-  Chatroom.attachRoutes(app)
-  DemoUpload.attachRoutes(app)
 
   // redirect routes
   Object.entries(redirectDict).forEach(([from, to]) =>
