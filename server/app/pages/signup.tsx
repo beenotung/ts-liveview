71a8dc4f119b7cbd8897764269415c020412a218
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-03-03 00:28:37 +0800

    finished interactive input validation for signup page

--- server/app/pages/signup.tsx
+++ server/app/pages/signup.tsx
@@ -1,4 +1,4 @@
-import { array, object, string } from 'cast.ts'
+import { object, string } from 'cast.ts'
 import { config } from '../../config.js'
 import { commonTemplatePageDesc } from '../components/common-template.js'
 import { Link, Redirect, Switch } from '../components/router.js'
@@ -14,7 +14,10 @@ import {
   googleLogo,
   instagramLogo,
 } from '../svgs/logo.js'
-import { proxy } from '../../../db/proxy.js'
+import { proxy, User } from '../../../db/proxy.js'
+import { ServerMessage } from '../../../client/types.js'
+import { is_email } from '@beenotung/tslib'
+import { Raw } from '../components/raw.js'
 
 let style = Style(/* css */ `
 .or-line::before,
@@ -81,27 +84,53 @@ function SignUpForm() {
       <label>
         Email
         <div class="input-container">
-          <input name="email" type="email" />
+          <input
+            name="email"
+            type="email"
+            oninput="emit('/register/check-email', this.value)"
+          />
         </div>
+        <p id="emailMsg"></p>
       </label>
-      <p>This email is invalid</p>
-      <p>This email is already taken</p>
       <label>
         Password
         <div className="input-container">
-          <input name="password" type="password" />
+          <input
+            name="password"
+            type="password"
+            oninput="emit('/register/check-password', this.value);this.form.confirm_password.value=''"
+          />
         </div>
+        <p id="passwordMsg"></p>
       </label>
-      <p>Password is too short</p>
-      <p>Password may be compromised</p>
-      <p>Password is strong</p>
       <label>
         Confirm password
         <div className="input-container">
-          <input name="confirm_password" type="password" />
+          <input
+            name="confirm_password"
+            type="password"
+            oninput="checkPassword(this.form)"
+          />
         </div>
       </label>
-      <p>Password not matched</p>
+      <p id="confirmPasswordMsg"></p>
+      {Raw(/* html */ `<script>
+function checkPassword (form) {
+  let c = form.confirm_password.value
+  if (c.length == 0) {
+    confirmPasswordMsg.textContent = ''
+    return
+  }
+  let p = form.password.value
+  if (p != c) {
+    confirmPasswordMsg.textContent = 'Password not matched'
+    confirmPasswordMsg.style.color = 'red'
+    return
+  }
+  confirmPasswordMsg.textContent = 'Password matched'
+  confirmPasswordMsg.style.color = 'green'
+}
+</script>`)}
       <input type="submit" value="Submit" />
     </form>
   )
@@ -124,101 +153,211 @@ function Submit(_attrs: {}, context: Context) {
   )
 }
 
+type ValidateResult =
+  | { type: 'error'; text: string; extra?: string }
+  | {
+      type: 'found'
+      text: string
+      user: User
+      extra?: string
+    }
+  | { type: 'ok'; text: string }
+
 let minUsername = 1
 let maxUsername = 32
 
-function CheckUsername(_: {}, context: WsContext) {
-  console.log('check username', context.args)
-  let username = context.args?.[0]
-  if (typeof username !== 'string' || username.length === 0) {
-    context.ws.send(['batch', [['update-text', '#usernameMsg', ``]]])
-    throw EarlyTerminate
-  }
+function validateUsername(username: string): ValidateResult {
   if (username.length < minUsername) {
-    context.ws.send([
-      'batch',
-      [
-        [
-          'update-text',
-          '#usernameMsg',
-          `username "${username}" is too short, need ${
-            minUsername - username.length
-          } more characters`,
-        ],
-        ['update-attrs', '#usernameMsg', { style: 'color:red' }],
-      ],
-    ])
-    throw EarlyTerminate
+    let diff = minUsername - username.length
+    return {
+      type: 'error' as const,
+      text: `username "${username}" is too short, need ${diff} more characters`,
+    }
   }
+
   if (username.length > maxUsername) {
-    context.ws.send([
-      'batch',
-      [
-        [
-          'update-text',
-          '#usernameMsg',
-          `username "${username}" is too long, need ${
-            username.length - maxUsername
-          } less characters`,
-        ],
-        ['update-attrs', '#usernameMsg', { style: 'color:red' }],
-      ],
-    ])
-    throw EarlyTerminate
+    let diff = username.length - maxUsername
+    return {
+      type: 'error' as const,
+      text: `username "${username}" is too long, need ${diff} less characters`,
+    }
   }
+
+  if (username.replace(/badminton/g, '').includes('admin')) {
+    return {
+      type: 'error' as const,
+      text: `username cannot contains "admin"`,
+    }
+  }
+
   let excludedChars = Array.from(
-    new Set(username.replace(/[a-z0-9]/g, '')),
+    new Set(username.replace(/[a-z0-9_]/g, '')),
   ).join('')
   if (excludedChars.length > 0) {
-    context.ws.send([
-      'batch',
-      [
-        [
-          'update-text',
-          '#usernameMsg',
-          `username should only consist of english letters [a-z] and digits [0-9], hyphen [-] and underscore [_] are also allowed.`,
-        ],
-        ['update-attrs', '#usernameMsg', { style: 'color:#a00' }],
-        [
-          'append',
-          '#usernameMsg',
-          [
-            'p',
-            { style: 'color:red' },
-            [`But ${JSON.stringify(excludedChars)} could not be used.`],
-          ],
-        ],
-      ],
-    ])
-    throw EarlyTerminate
+    return {
+      type: 'error' as const,
+      text: `username cannot contains "${excludedChars}"`,
+      extra: `username should only consist of english letters [a-z] and digits [0-9], underscore [_] is also allowed`,
+    }
   }
-  if (find(proxy.user, { username })) {
-    context.ws.send([
-      'batch',
-      [
-        [
-          'update-text',
-          '#usernameMsg',
-          `username "${username}" is already used`,
-        ],
-        ['update-attrs', '#usernameMsg', { style: 'color:red' }],
-      ],
-    ])
-    throw EarlyTerminate
-  }
-  context.ws.send([
-    'batch',
-    [
-      ['update-text', '#usernameMsg', `username "${username}" is available`],
-      ['update-attrs', '#usernameMsg', { style: 'color:green' }],
-    ],
-  ])
 
+  let user = find(proxy.user, { username })
+  if (user) {
+    return {
+      type: 'found' as const,
+      text: `username "${username}" is already used`,
+      user,
+    }
+  }
+
+  return {
+    type: 'ok' as const,
+    text: `username "${username}" is available`,
+  }
+}
+
+let minPassword = 6
+let maxPassword = 256
+
+function validatePassword(password: string): ValidateResult {
+  if (password.length < minPassword) {
+    let diff = minPassword - password.length
+    return {
+      type: 'error' as const,
+      text: `the password is too short, need ${diff} more characters`,
+    }
+  }
+
+  if (password.length > maxPassword) {
+    let diff = password.length - maxPassword
+    return {
+      type: 'error' as const,
+      text: `the password is too long, need ${diff} less characters`,
+    }
+  }
+  return { type: 'ok' as const, text: 'password is acceptable' }
+}
+
+function validateEmail(email: string): ValidateResult {
+  if (!is_email(email)) {
+    return { type: 'error', text: 'invalid email' }
+  }
+
+  let user = find(proxy.user, { email })
+  if (user) {
+    return {
+      type: 'found' as const,
+      text: `email "${email}" is already used`,
+      user,
+    }
+  }
+
+  return { type: 'ok', text: `email "${email}" is valid` }
+}
+
+function validateInput(input: {
+  context: WsContext
+  field: string
+  value: string
+  selector: string
+  validate: (value: string) => ValidateResult
+}) {
+  let { context, value, selector } = input
+
+  if (typeof value !== 'string' || value.length == 0) {
+    context.ws.send(['update-text', selector, ``])
+    throw EarlyTerminate
+  }
+
+  let result = input.validate(value)
+
+  if (result.type == 'ok') {
+    context.ws.send([
+      'batch',
+      [
+        ['update-text', selector, result.text],
+        ['update-attrs', selector, { style: 'color:green' }],
+      ],
+    ])
+
+    throw EarlyTerminate
+  }
+
+  if (result.type == 'error' || result.type == 'found') {
+    sendInvalidMessage({
+      context,
+      selector,
+      text: result.text,
+      extra: result.extra,
+    })
+  }
+}
+
+function sendInvalidMessage(input: {
+  context: WsContext
+  text: string
+  selector: string
+  extra?: string
+}) {
+  let { context, text, selector, extra } = input
+
+  let messages: ServerMessage[] = [
+    ['update-text', selector, text],
+    ['update-attrs', selector, { style: 'color:red' }],
+  ]
+  if (extra) {
+    messages.push([
+      'append',
+      selector,
+      [
+        'span',
+        { style: 'color:darkred;display:block;margin-top:0.25rem' },
+        [extra],
+      ],
+    ])
+  }
+  context.ws.send(['batch', messages])
   throw EarlyTerminate
+}
+
+function CheckUsername(_: {}, context: WsContext) {
+  let username = context.args?.[0] as string
+  username = username?.toLocaleLowerCase()
+  validateInput({
+    context,
+    value: username,
+    field: 'username',
+    selector: '#usernameMsg',
+    validate: validateUsername,
+  })
+}
+
+function CheckPassword(_: {}, context: WsContext) {
+  let password = context.args?.[0] as string
+  validateInput({
+    context,
+    value: password,
+    field: 'password',
+    selector: '#passwordMsg',
+    validate: validatePassword,
+  })
+}
+
+function CheckEmail(_: {}, context: WsContext) {
+  let email = context.args?.[0] as string
+  validateInput({
+    context,
+    value: email,
+    field: 'email',
+    selector: '#emailMsg',
+    validate: validateEmail,
+  })
 }
 
 export default Switch({
   '/register': SignUpPage,
   '/register/check-username': <CheckUsername />,
+  '/register/check-password': <CheckPassword />,
+  '/register/check-email': <CheckEmail />,
   '/register/submit': <Submit />,
 })
