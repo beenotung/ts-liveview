dd89aed100b4988fbcb20de3e0effe9a2634341d
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-03-23 22:53:54 +0800

    remove "commonTemplatePageDesc" as wrapper of commonTemplatePageText

--- server/app/components/common-template.tsx
+++ server/app/components/common-template.tsx
@@ -1,6 +1,2 @@
-import { o } from '../jsx/jsx.js'
-
 export let commonTemplatePageText =
   'This page serve as template of common use case.'
-
-export let commonTemplatePageDesc = <p>{commonTemplatePageText}</p>
