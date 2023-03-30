75771107cc34b3027c307098eb5f5dcf29ae72cd
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-03-30 19:59:57 +0800

    generation jwt and store in cookie

---
+++ server/app/jwt.ts
@@ -0,0 +1,14 @@
+import jwt from 'jwt-simple'
+import { config } from '../config.js'
+
+export type JwtPayload = {
+  id: number
+}
+
+export function encodeJwt(payload: JwtPayload): string {
+  return jwt.encode(payload, config.cookie_secret)
+}
+
+export function decodeJwt(token: string) {
+  return jwt.decode(token, config.cookie_secret)
+}
