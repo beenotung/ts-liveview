356d8e208f7af93fe8262a7da8b7b7b8cf5c3791
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-04-29 19:19:10 +0800

    pre-compute the user agent stats to speed up querying

---
+++ db/migrations/20230429112109_auto-migrate.ts
@@ -0,0 +1,42 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+
+  if (!(await knex.schema.hasTable('ua_type'))) {
+    await knex.schema.createTable('ua_type', table => {
+      table.increments('id')
+      table.text('name').notNullable().unique()
+      table.integer('count').notNullable()
+      table.timestamps(false, true)
+    })
+  }
+
+  if (!(await knex.schema.hasTable('ua_bot'))) {
+    await knex.schema.createTable('ua_bot', table => {
+      table.increments('id')
+      table.text('name').notNullable().unique()
+      table.integer('count').notNullable()
+      table.timestamps(false, true)
+    })
+  }
+  await knex.raw('alter table `user_agent` add column `ua_type_id` integer null references `ua_type`(`id`)')
+  await knex.raw('alter table `user_agent` add column `ua_bot_id` integer null references `ua_bot`(`id`)')
+
+  if (!(await knex.schema.hasTable('ua_stat'))) {
+    await knex.schema.createTable('ua_stat', table => {
+      table.increments('id')
+      table.integer('last_request_log_id').notNullable()
+      table.timestamps(false, true)
+    })
+  }
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists('ua_stat')
+  await knex.raw('alter table `user_agent` drop column `ua_bot_id`')
+  await knex.raw('alter table `user_agent` drop column `ua_type_id`')
+  await knex.schema.dropTableIfExists('ua_bot')
+  await knex.schema.dropTableIfExists('ua_type')
+}
