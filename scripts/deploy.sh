9ed4403bca6f521b7af2a95be4b0558abe7855d8
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-06-22 03:34:26 +0800

    improve deploy script format; support MODE=first to setup data folder and pm2 task

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,37 +3,55 @@ set -e
 set -o pipefail
 
 source scripts/config
+
+set -x
+
 if [ "$MODE" == "quick" ]; then
-rsync -SavLPz \
-  public \
-  build \
-  dist \
-  "$user@$host:$root_dir"
-ssh "$user@$host" " \
-source ~/.nvm/nvm.sh \
-&& pm2 reload $pm2_name \
-"
+  rsync -SavLPz \
+    public \
+    build \
+    dist \
+    "$user@$host:$root_dir"
+  ssh "$user@$host" "
+    set -e
+    source ~/.nvm/nvm.sh
+    pm2 reload $pm2_name
+  "
 else
-npm run build
-rsync -SavLPz \
-  public \
-  build \
-  dist \
-  package.json \
-  "$user@$host:$root_dir"
-rsync -SavLPz \
-  db/package.json \
-  db/migrations \
-  db/knexfile.ts \
-  db/db.ts \
-  "$user@$host:$root_dir/db"
-ssh "$user@$host" " \
-source ~/.nvm/nvm.sh \
-&& set -x \\
-&& cd $root_dir \
-&& pnpm i -r \
-&& cd db \
-&& npm run migrate \
-&& pm2 reload $pm2_name \
-"
+  npm run build
+  rsync -SavLPz \
+    public \
+    build \
+    dist \
+    package.json \
+    "$user@$host:$root_dir"
+  rsync -SavLPz \
+    db/package.json \
+    db/migrations \
+    db/knexfile.ts \
+    db/db.ts \
+    "$user@$host:$root_dir/db"
+  if [ "$MODE" == "first" ]; then
+    ssh "$user@$host" "
+      set -e
+      cd $root_dir
+      mkdir -p data
+      mkdir -p db
+      cd db
+      ln -sf ../data
+    "
+    pm2_cmd="cd $root_dir && pm2 start --name $pm2_name dist/server/index.js"
+  else
+    pm2_cmd="pm2 reload $pm2_name"
+  fi
+  ssh "$user@$host" "
+    set -e
+    source ~/.nvm/nvm.sh
+    set -x
+    cd $root_dir
+    pnpm i -r
+    cd db
+    npm run migrate
+    $pm2_cmd
+  "
 fi
