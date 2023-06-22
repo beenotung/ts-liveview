9176841bdcb9bc72bb998a00615da2a13b016e05
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-06-22 14:43:27 +0800

    feat: add update-message compoment to provide ack when ws update is done

---
+++ server/app/components/update-message.tsx
@@ -0,0 +1,48 @@
+import { DynamicContext, WsContext } from '../context.js'
+import { o } from '../jsx/jsx.js'
+import { toLocaleDateTimeString } from './datetime.js'
+
+export let UpdateMessageStyle = /* css */ `
+.update-message {
+  color: green;
+  font-size: smaller;
+}
+`
+
+export function UpdateMessage(attrs: { id: string }) {
+  return <p id={attrs.id} class="update-message"></p>
+}
+
+export function updateMessageText(
+  attrs: {
+    label: string
+  },
+  context: DynamicContext,
+): string {
+  let { label } = attrs
+  let time = toLocaleDateTimeString(Date.now(), context, {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false,
+  })
+  return `[${time}] Updated ${label}.`
+}
+
+export function newUpdateMessage(attrs?: { id?: string }) {
+  let id = attrs?.id || uniqueId()
+  let node = UpdateMessage({ id })
+  let selector = '#' + id
+  function sendWsUpdate(attrs: { label: string }, context: WsContext) {
+    let text = updateMessageText(attrs, context)
+    context.ws.send(['update-text', selector, text])
+  }
+  return { node, sendWsUpdate }
+}
+
+let nextId = 1
+function uniqueId() {
+  let id = 'update-message-' + nextId
+  nextId++
+  return id
+}
