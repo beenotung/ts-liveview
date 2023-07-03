8b7c656b2de3fdda0631d2c54ea482f6552b313d
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-07-04 02:35:22 +0800

    chore: improve edge case handling when getting status code from error

--- server/app/components/error.ts
+++ server/app/components/error.ts
@@ -13,7 +13,7 @@ export let ErrorStyle = /* css */ `
 
 export function renderError(error: unknown, context: Context): VNode {
   if (context.type === 'express' && !context.res.headersSent && error) {
-    let code = (error as any).statusCode || (error as any).status || 500
+    let code = getErrorStatusCode(error)
     context.res.status(code)
   }
   return ['p.error', {}, [String(error)]]
@@ -22,3 +22,13 @@ export function renderError(error: unknown, context: Context): VNode {
 export function renderErrorNode(error: ErrorNode, context: Context): Node {
   return ['p.error', {}, [error.node]]
 }
+
+function getErrorStatusCode(error: unknown): number {
+  if (error != null && typeof error == 'object') {
+    let object = error as any
+    return object.statusCode || object.status || defaultErrorStatusCode
+  }
+  return defaultErrorStatusCode
+}
+
+const defaultErrorStatusCode = 500
