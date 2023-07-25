f8c395054966095083f6a9804d813560f811ffa9
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-07-26 02:55:59 +0800

    chore: change font-size and margin of headings

--- server/app/app-style.tsx
+++ server/app/app-style.tsx
@@ -13,6 +13,14 @@ body {
   font-family: sans-serif;
 }
 
+h1 { font-size: 2rem }
+h2 { font-size: 1.8rem }
+h3 { font-size: 1.75rem }
+h4 { font-size: 1.5rem }
+h5 { font-size: 1.25rem }
+h6 { font-size: 1.2rem }
+h1,h2,h3,h4,h5,h6 { margin: 1em 0 0.5em }
+
 .text-no-wrap {
   display: inline-block;
   width: max-content;
