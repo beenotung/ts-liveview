dc25b5c28860ee8d971593161127da59275d84bc
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-07-30 07:39:03 +0800

    feat: add user avatar

---
+++ db/migrations/20230729233254_auto-migrate.ts
@@ -0,0 +1,11 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw('alter table `user` add column `avatar` text null')
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.raw('alter table `user` drop column `avatar`')
+}
