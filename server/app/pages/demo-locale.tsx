6ae39709c354951b6f9c73c345f9d871c07e1376
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-09-09 04:57:25 +0800

    demo locale handling for page title and description

--- server/app/pages/demo-locale.tsx
+++ server/app/pages/demo-locale.tsx
@@ -1,15 +1,16 @@
+import { title } from '../../config.js'
 import { isPreferZh } from '../components/locale.js'
 import SourceCode from '../components/source-code.js'
 import { PickLanguage } from '../components/ui-language.js'
 import { Context, getContextLanguage, getContextTimezone } from '../context.js'
 import { o } from '../jsx/jsx.js'
+import { Node } from '../jsx/types.js'
+import { Routes } from '../routes.js'
 
-function DemoLocale(_attrs: {}, context: Context) {
-  let lang = getContextLanguage(context) || 'unknown'
-  let timezone = getContextTimezone(context) || 'unknown'
+function DemoLocale(attrs: { main: Node }) {
   return (
     <div id="demo-locale">
-      {isPreferZh(context) ? Zh({ lang, timezone }) : En({ lang, timezone })}
+      {attrs.main}
       <SourceCode page="demo-locale.tsx" />
     </div>
   )
@@ -18,7 +19,7 @@ function DemoLocale(_attrs: {}, context: Context) {
 function En(attrs: { lang: string; timezone: string }) {
   return (
     <>
-      <h1>Demo Locale</h1>
+      <h1>Locale Demo</h1>
       <p>
         This page demo supporting users with multiple langues and timezones.
       </p>
@@ -43,4 +44,26 @@ function Zh(attrs: { lang: string; timezone: string }) {
   )
 }
 
-export default DemoLocale
+let routes: Routes = {
+  '/locale': {
+    menuText: 'Locale',
+    resolve(context) {
+      let lang = getContextLanguage(context) || 'unknown'
+      let timezone = getContextTimezone(context) || 'unknown'
+      return isPreferZh(context)
+        ? {
+            title: title('本地化設置示範'),
+            description: '示範支援多種語言和時區 (i18n)',
+            node: <DemoLocale main={Zh({ lang, timezone })} />,
+          }
+        : {
+            title: title('Locale Demo'),
+            description:
+              'Locale demo for multiple languages and timezone (i18n)',
+            node: <DemoLocale main={En({ lang, timezone })} />,
+          }
+    },
+  },
+}
+
+export default { routes }
