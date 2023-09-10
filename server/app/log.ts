efd239af35ccab793e1e9261e4800e98fabd21ad
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-09-11 04:55:12 +0800

    chore: extract request logging call to helper function logRequest() to centralize metadata extraction logics

---
+++ server/app/log.ts
@@ -0,0 +1,10 @@
+import type { Request } from 'express'
+import { storeRequestLog } from '../../db/store.js'
+
+export function logRequest(req: Request, method: string, url: string) {
+  storeRequestLog({
+    method,
+    url,
+    user_agent: req.headers['user-agent'] || null,
+  })
+}
