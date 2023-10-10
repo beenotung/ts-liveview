83b589a55ca5c1e67b5f51194c61eb998c4fd635
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-10-10 11:18:06 +0800

    fix grep usage for linux

--- scripts/caddy-run.sh
+++ scripts/caddy-run.sh
@@ -4,7 +4,7 @@ set -o pipefail
 
 source .env
 [ -z "$PORT" ] && PORT=8100
-IP="$(ifconfig | grep -oE '192.\d+.\d+.\d+' | grep -v 255)"
+IP="$(ifconfig | grep -oE '192.[0-9]+.[0-9]+.[0-9]+' | grep -v 255)"
 TO="http://$IP:$PORT"
 echo "proxy to: $TO"
 caddy reverse-proxy --to "$TO" --from "$IP" --internal-certs
