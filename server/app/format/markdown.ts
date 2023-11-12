abbd9ed46a9db890023858e6159daab900fb1693
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-11-13 01:36:52 +0800

    chore: remove deprecated options of marked

--- server/app/format/markdown.ts
+++ server/app/format/markdown.ts
@@ -1,9 +1,5 @@
 import { marked } from 'marked'
 
 export function markdownToHtml(text: string): string {
-  return marked(text, {
-    mangle: false,
-    headerIds: false,
-    headerPrefix: undefined,
-  })
+  return marked(text)
 }
