809276cf87517affb4616d8b0099cae2ceed5f53
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-11-28 01:05:04 +0800

    chore: update better-sqlite3-proxy to use seedRow() helper function

--- db/seed.ts
+++ db/seed.ts
@@ -1,10 +1,15 @@
+import { seedRow } from 'better-sqlite3-proxy'
 import { proxy } from './proxy'
 
 // This file serve like the knex seed file.
 //
 // You can setup the database with initial config and sample data via the db proxy.
 
+seedRow(proxy.method, { method: 'GET' })
+seedRow(proxy.method, { method: 'POST' })
+seedRow(proxy.method, { method: 'ws' })
+
 console.log(
-  'received methods:',
+  'request methods:',
   proxy.method.map(row => row.method),
 )
