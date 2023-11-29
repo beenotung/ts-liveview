3924a841b6766ddad482946621aac3f7f74ccb69
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-11-29 16:25:55 +0800

    chore: use webp in compressPhotos()

--- client/image.ts
+++ client/image.ts
@@ -1,10 +1,15 @@
 import { format_byte } from '@beenotung/tslib/format.js'
 import { compressMobilePhoto, dataURItoFile } from '@beenotung/tslib/image.js'
+import { KB } from '@beenotung/tslib/size'
 
 function compressPhotos(files: FileList) {
   return Promise.all(
     Array.from(files, async file => {
-      let dataUrl = await compressMobilePhoto({ image: file })
+      let dataUrl = await compressMobilePhoto({
+        image: file,
+        maximumSize: 300 * KB,
+        mimeType: 'image/webp',
+      })
       file = dataURItoFile(dataUrl, file)
       return { dataUrl, file }
     }),
