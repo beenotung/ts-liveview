de29cba6c2a419bbf2dc6862fcd831e3e74a2e7e
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-11-30 17:13:42 +0800

    patch: mark user(username,email,tel) unique

---
+++ db/migrations/20231130091225_auto-migrate.ts
@@ -0,0 +1,19 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('user', table => {
+    table.unique(['username'])
+    table.unique(['email'])
+    table.unique(['tel'])
+  })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('user', table => {
+    table.dropUnique(['tel'])
+    table.dropUnique(['email'])
+    table.dropUnique(['username'])
+  })
+}
