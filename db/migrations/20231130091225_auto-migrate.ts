d43edbcc8bf2851cb1b1dceb10eb6a9d5e67b6d4
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-12-03 23:22:20 +0800

    chore: format with prettier

--- db/migrations/20231130091225_auto-migrate.ts
+++ db/migrations/20231130091225_auto-migrate.ts
@@ -1,5 +1,4 @@
-import { Knex } from "knex";
-
+import { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable('user', table => {
@@ -8,7 +7,6 @@ export async function up(knex: Knex): Promise<void> {
     table.unique(['tel'])
   })
 }
-
 
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.alterTable('user', table => {
