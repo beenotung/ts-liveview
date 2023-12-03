606b1cb3ecfdb618ccde834df8abdfc7ab0cd662
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-12-03 23:22:20 +0800

    chore: format with prettier

--- db/migrations/20230729233254_auto-migrate.ts
+++ db/migrations/20230729233254_auto-migrate.ts
@@ -1,10 +1,8 @@
-import { Knex } from "knex";
-
+import { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
   await knex.raw('alter table `user` add column `avatar` varchar(256) null')
 }
-
 
 export async function down(knex: Knex): Promise<void> {
   await knex.raw('alter table `user` drop column `avatar`')
