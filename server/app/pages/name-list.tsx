f4398e0b8aa0edea7b4da4285c8a6005c06ad759
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-12-09 13:32:32 +0800

    docs: add desc and source code for the name-list demo docs: change name-list demo from string array to object array

--- server/app/pages/name-list.tsx
+++ server/app/pages/name-list.tsx
@@ -1,35 +1,60 @@
 import { title } from '../../config.js'
 import { mapArray } from '../components/fragment.js'
 import { newSingleFieldForm } from '../components/single-field-form.js'
+import SourceCode from '../components/source-code.js'
+import Style from '../components/style.js'
 import { o } from '../jsx/jsx.js'
 import { Routes } from '../routes.js'
+
+let style = Style(/* css */ `
+#name-list legend {
+  margin-bottom: 0.75rem;
+}
+`)
+
+let users = [
+  { id: 1, nickname: 'alice' },
+  { id: 2, nickname: 'bob' },
+  { id: 3, nickname: 'charlie' },
+]
 
 let Nickname = newSingleFieldForm({
   action: '/name-list/update',
   label: 'Nickname',
   name: 'nickname',
-  updateKeyName: 'index',
+  updateKeyName: 'id',
   updateValue(attrs, context) {
-    const { index, nickname } = attrs.input
-    names[+index] = nickname
+    const { id, nickname } = attrs.input
+    const index = +id - 1
+    users[index].nickname = nickname
   },
   renderUpdate(attrs, context) {
     return content
   },
 })
 
-let names = ['alice', 'bob', 'charlie']
-
 let content = (
-  <div>
+  <div id="name-list">
+    {style}
     <h1>List Editing Demo</h1>
+    <p>This page demo how to use multiple instances of SingleFieldForm.</p>
+    <p>Each instance can be distinguished by the "key" attribute.</p>
+    <p>
+      Example attribute value are index of array and primary key of database
+      record.
+    </p>
+    <p>Below are 3 instances of Nickname.Form mapped over an array of users.</p>
     <View />
+    <SourceCode page="name-list.tsx" />
   </div>
 )
 
 function View() {
-  return mapArray(names, (name, index) => (
-    <Nickname.Form key={index} value={name} />
+  return mapArray(users, user => (
+    <fieldset>
+      <legend>user {user.id}</legend>
+      <Nickname.Form key={user.id} value={user.nickname} />
+    </fieldset>
   ))
 }
 
