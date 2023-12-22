6f4188a7a6fb2333980b57d443e3e5a31d7a639b
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-12-22 18:55:54 +0800

    chore: remove deprecated express route handler chore: register verification routes

--- server/app/app.tsx
+++ server/app/app.tsx
@@ -42,7 +42,6 @@ import { WindowStub } from '../../client/internal.js'
 import { updateRequestSession } from '../../db/request-log.js'
 import { Link } from './components/router.js'
 import ErrorLog from './store/error-log.js'
-import VerificationCode from './pages/verification-code.js'
 
 if (config.development) {
   scanTemplateDir('template')
@@ -208,7 +207,6 @@ export function attachRoutes(app: Router) {
   // ajax/upload/middleware routes
   app.use(renewAuthCookieMiddleware)
   ErrorLog.attachRoutes(app)
-  VerificationCode.attachRoutes(app)
   Profile.attachRoutes(app)
 
   // redirect routes
