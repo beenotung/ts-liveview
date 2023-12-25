0215824e6734a7a84802c2188d2c5214ec84d5a6
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-12-26 04:41:10 +0800

    feat: add navigation animation for ionic template

--- server/app/styles/mobile-style.ts
+++ server/app/styles/mobile-style.ts
@@ -1,3 +1,26 @@
 export let MobileStyle = /* css */ `
-
+.page {
+  transition: all 0.3s ease;
+  opacity: 1;
+}
+.page.hide {
+  opacity: 0;
+}
+@media (prefers-reduced-motion: no-preference) {
+  .page {
+    transform: translate(0,0);
+  }
+  .ios .page.hide {
+    transform: translate(100%,0);
+  }
+  .ios .back .page.hide {
+    transform: translate(-100%,0);
+  }
+  .md .page.hide {
+    transform: translate(0,100%);
+  }
+  .md .back .page.hide {
+    transform: translate(0,-100%);
+  }
+}
 `
