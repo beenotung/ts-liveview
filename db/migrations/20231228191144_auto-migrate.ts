39c32a0a06fa767f5110f15818de32207817d663
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-12-29 03:51:17 +0800

    feat: finished email verification for login flow

---
+++ db/migrations/20231228191144_auto-migrate.ts
@@ -0,0 +1,31 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  {
+    const rows = await knex.select('id', 'username').from('user')
+    await knex.schema.alterTable('user', table => table.dropUnique(['username']))
+    await knex.raw('alter table `user` drop column `username`')
+    await knex.raw("alter table `user` add column `username` text null")
+    await knex.schema.alterTable('user', table => table.unique(['username']))
+    for (let row of rows) {
+      await knex('user').update({ username: row.username }).where({ id: row.id })
+    }
+  }
+  // FIXME: alter column (verification_code.passcode) to be non-nullable not supported in sqlite
+  // you may set it to be non-nullable with sqlite browser manually
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  {
+    const rows = await knex.select('id', 'passcode').from('verification_code')
+    await knex.raw('alter table `verification_code` drop column `passcode`')
+    await knex.raw("alter table `verification_code` add column `passcode` text null")
+    for (let row of rows) {
+      await knex('verification_code').update({ passcode: row.passcode }).where({ id: row.id })
+    }
+  }
+  // FIXME: alter column (user.username) to be non-nullable not supported in sqlite
+  // you may set it to be non-nullable with sqlite browser manually
+}
