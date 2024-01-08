2a27c725864931b21433af4eeb76233c9df12156
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-01-08 17:16:01 +0800

    chore: move or-line style to common space to be shared between login and register pages

--- server/app/styles/form-style.ts
+++ server/app/styles/form-style.ts
@@ -3,4 +3,12 @@ form .input-container {
   margin-top: 0.25rem;
   margin-bottom: 0.75rem;
 }
+
+.or-line::before,
+.or-line::after {
+  content: '';
+  flex: 1;
+  border-bottom: 1px solid #888;
+  margin: 1rem 0.75rem;
+}
 `
