4616e0da3df81d7923665b4ba2bd158aa41cbc40
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-01-09 22:26:45 +0800

    chore: remove ionic and hybrid pages for web-template
    chore: remove ionic components for web-template

--- server/app/routes.tsx
+++ server/app/routes.tsx
@@ -1,7 +1,3 @@
-import AppNotice from './pages/app-notice.js'
-import AppChat from './pages/app-chat.js'
-import AppSettings from './pages/app-settings.js'
-import AppMore from './pages/app-more.js'
 import Privacy from './pages/privacy.js'
 import { capitalize } from '@beenotung/tslib/string.js'
 import { Router } from 'url-router.ts'
@@ -18,11 +14,7 @@ import NotFoundPageRoute from './pages/not-found.js'
 import { then } from '@beenotung/tslib/result.js'
 import type { MenuRoute } from './components/menu'
 import DemoPlugin from './pages/demo-plugin.js'
-import AppHome from './pages/app-home.js'
-import AppAbout from './pages/app-about.js'
-import AppCharacter from './pages/app-character.js'
 import type { renderWebTemplate } from '../../template/web.js'
-import type { renderIonicTemplate } from '../../template/ionic.js'
 import { VNode } from '../../client/jsx/types.js'
 import { evalAttrsLocale } from './components/locale.js'
 
@@ -37,7 +29,7 @@ const StreamingByDefault = true
 
 export type PageRoute = PageRouteOptions & (StaticPageRoute | DynamicPageRoute)
 
-type TemplateFn = typeof renderWebTemplate | typeof renderIonicTemplate
+type TemplateFn = typeof renderWebTemplate
 
 type RenderOptions = {
   layout_type?: LayoutType
@@ -78,13 +70,6 @@ let routeDict = {
   ...DemoPlugin.routes,
   ...UILanguage.routes,
   ...UserAgents.routes,
-  ...AppHome.routes,
-  ...AppCharacter.routes,
-  ...AppAbout.routes,
-  ...AppChat.routes,
-  ...AppNotice.routes,
-  ...AppMore.routes,
-  ...AppSettings.routes,
   ...Privacy.routes,
 } satisfies Routes
 
