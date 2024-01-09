538a790f32c88061d7094ede2f075abc4919133c
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-01-09 22:26:45 +0800

    chore: remove ionic and hybrid pages for web-template

--- server/app/routes.tsx
+++ server/app/routes.tsx
@@ -20,11 +20,7 @@ import Profile from './pages/profile.js'
 import VerificationCode from './pages/verification-code.js'
 import type { MenuRoute } from './components/menu'
 import DemoToast from './pages/demo-toast.js'
-import AppHome from './pages/app-home.js'
-import AppAbout from './pages/app-about.js'
-import AppCharacter from './pages/app-character.js'
 import type { renderWebTemplate } from '../../template/web.js'
-import type { renderIonicTemplate } from '../../template/ionic.js'
 import { VNode } from '../../client/jsx/types.js'
 import { EarlyTerminate, MessageException } from '../exception.js'
 import { renderError } from './components/error.js'
@@ -41,7 +37,7 @@ const StreamingByDefault = true
 
 export type PageRoute = PageRouteOptions & (StaticPageRoute | DynamicPageRoute)
 
-type TemplateFn = typeof renderWebTemplate | typeof renderIonicTemplate
+type TemplateFn = typeof renderWebTemplate
 
 type RenderOptions = {
   layout_type?: LayoutType
@@ -98,13 +94,6 @@ let routeDict = {
   ...Register.routes,
   ...Profile.routes,
   ...VerificationCode.routes,
-  ...AppHome.routes,
-  ...AppCharacter.routes,
-  ...AppAbout.routes,
-  ...AppChat.routes,
-  ...AppNotice.routes,
-  ...AppMore.routes,
-  ...AppSettings.routes,
 } satisfies Routes
 
 export let redirectDict: Record<string, string> = {
