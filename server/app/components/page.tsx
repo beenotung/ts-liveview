4ab1a18d0f3ab35671b266016917b2c4231cb980
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-01-09 22:26:45 +0800

    chore: remove ionic and hybrid pages for web-template
    chore: remove ionic components for web-template

--- server/app/components/page.tsx
+++ server/app/components/page.tsx
@@ -1,18 +1,6 @@
 import { config, LayoutType } from '../../config.js'
 import { o } from '../jsx/jsx.js'
 import { Node, NodeList } from '../jsx/types.js'
-import { IonBackButton } from './ion-back-button.js'
-
-export type ThemeColor =
-  | 'primary'
-  | 'secondary'
-  | 'tertiary'
-  | 'success'
-  | 'warning'
-  | 'danger'
-  | 'light'
-  | 'medium'
-  | 'dark'
 
 type PageAttrs = {
   style?: string
@@ -21,42 +9,7 @@ type PageAttrs = {
   children?: NodeList
   backText?: string
   backHref?: string | false
-  backColor?: ThemeColor
   class?: string
-  color?: ThemeColor
-}
-
-function IonicPage(attrs: PageAttrs) {
-  let backHref = attrs.backHref ?? '/'
-  return (
-    <>
-      {backHref || attrs.title ? (
-        <ion-header>
-          <ion-toolbar color={attrs.color}>
-            {backHref ? (
-              <IonBackButton
-                href={backHref}
-                backText={attrs.backText}
-                color={attrs.backColor}
-              />
-            ) : null}
-            {attrs.title ? (
-              <ion-title role="heading" aria-level="1">
-                {attrs.title}
-              </ion-title>
-            ) : null}
-          </ion-toolbar>
-        </ion-header>
-      ) : null}
-      <ion-content
-        id={attrs.id}
-        class={attrs.class ?? 'ion-padding'}
-        style={attrs.style}
-      >
-        {attrs.children ? [attrs.children] : null}
-      </ion-content>
-    </>
-  )
 }
 
 function WebPage(attrs: PageAttrs) {
@@ -68,17 +21,4 @@ function WebPage(attrs: PageAttrs) {
   )
 }
 
-export let is_ionic = config.layout_type === LayoutType.ionic
-
-export let Page = is_ionic ? IonicPage : WebPage
-
-type Content = Node | (() => Node)
-
-export function Content(attrs: { ionic?: Content; web?: Content }): Node {
-  let content =
-    config.layout_type === LayoutType.ionic ? attrs.ionic : attrs.web
-  if (typeof content === 'function') {
-    return content()
-  }
-  return content
-}
+export let Page = WebPage
