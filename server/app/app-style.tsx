baf4274cf5d9561b2cd277a6ef6b64de3af08fc0
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-01-09 22:26:45 +0800

    chore: remove ionic and hybrid pages for web-template
    chore: remove ionic components for web-template
    
    chore: retain IonButton for the report-content page

--- server/app/app-style.tsx
+++ server/app/app-style.tsx
@@ -4,7 +4,6 @@ import Style from './components/style.js'
 import { UpdateMessageStyle } from './components/update-message.js'
 import { CommonStyle } from './styles/common-style.js'
 import { FormStyle } from './styles/form-style.js'
-import { MobileStyle } from './styles/mobile-style.js'
 import { WebStyle } from './styles/web-style.js'
 
 let appStyle = /* css */ `
@@ -16,5 +15,3 @@ ${FormStyle}
 `
 
 export let webAppStyle = Style(appStyle + '\n' + WebStyle)
-
-export let ionicAppStyle = Style(appStyle + '\n' + MobileStyle)
