2c6cea6fef5ea16e32fc75843e62cab457a3def8
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-01-11 21:54:42 +0800

    chore: update new-routes.sh to use template file in tsx for easier editing

--- scripts/new-route.sh
+++ scripts/new-route.sh
@@ -2,78 +2,47 @@
 set -e
 set -o pipefail
 
-type="$(sed -nr 's/\s+layout_type: LayoutType\.(\w+).*/\1/p' server/config.ts)"
-echo "page type: $type"
-
 if [ $# == 0 ]; then
   read -p "page name: " name
 else
-  name="$1"
+  name="$@"
 fi
 
-Name=$(echo "$name" | sed 's/^\(.\)/\U\1/' | sed 's/-\(.\)/\U\1/g')
-Title=$(echo "$name" | sed 's/^\(.\)/\U\1/' | sed 's/-\(.\)/ \U\1/g')
+# e.g. user agents
+name="$(echo "$name" | sed -r 's/^ +//g' | sed -r 's/ +$//g')"
 
-file="server/app/pages/$name.tsx"
+# e.g. User Agents
+title="$(echo "$name" | sed -r 's/-/ /g' | sed -r 's/ (\w)/ \U\1/g' | sed -r 's/^(\w)/\U\1/')"
+
+# e.g. UserAgents
+id="$(echo "$title" | sed -r 's/ //g')"
+
+# e.g. user-agents
+url="$(echo "$name" | sed -r 's/ /-/g' | sed -r 's/(\w)/\l\1/g')"
+
+file="server/app/pages/$url.tsx"
 
 if [ -f "$file" ]; then
   echo >&2 "File already exist: $file"
   read -p "Overwrite? [y/N] " ans
-  if [[ $ans != y* ]]; then
+  if [[ "$ans" != y* ]]; then
     echo >&2 "Cancelled."
     exit
   fi
 fi
 
-if [ "$type" == "ionic" ]; then
-  source ./scripts/route-template-ionic.sh
-else
-  source ./scripts/route-template-web.sh
-fi
-source ./scripts/route-template-hybrid.sh
-
-echo "import { o } from '../jsx/jsx.js'
-import { Routes } from '../routes.js'
-import { apiEndpointTitle, title } from '../../config.js'
-import Style from '../components/style.js'
-import { Context } from '../context.js'
-import { mapArray } from '../components/fragment.js'
-$template_import
-let pageTitle = '$Title'
-
-let style = Style(/* css */ \`
-#$name {
-
-}
-\`)
-$template_main
-function Submit() {
-  return 'TODO'
-}
-
-let routes: Routes = {
-  '/$name': {
-    title: title(pageTitle),
-    description: 'TODO',
-    menuText: pageTitle,
-    node: page,
-  },
-  '/$name/submit': {
-    title: apiEndpointTitle,
-    description: 'TODO',
-    node: <Submit/>,
-    streaming: false,
-  },
-}
-
-export default { routes }" > "$file"
+cat "server/app/pages/template.tsx" \
+  | sed "s/__id__/$id/" \
+  | sed "s/__title__/$title/" \
+  | sed "s/__url__/$url/" \
+  > "$file"
 
 echo "saved to $file"
 code "$file"
 
 file="server/app/routes.tsx"
-echo "import $Name from './pages/$name.js'" > "$file.tmp"
+echo "import $id from './pages/$url.js'" > "$file.tmp"
 cat "$file" >> "$file.tmp"
 mv "$file.tmp" "$file"
-sed -i "s/let routeDict: Routes = {/let routeDict: Routes = {\n  ...$Name.routes,/" "$file"
+sed -i "s/let routeDict: Routes = {/let routeDict: Routes = {\n  ...$id.routes,/" "$file"
 echo "updated $file"
