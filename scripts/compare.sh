3d2af368bb4f351f629bf763d0ad943baa2e0e34
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-01-12 13:09:25 +0800

    chore: update compare.sh to clean up node_modules and built files before calling meld

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -3,4 +3,24 @@
 ## you may change the path according to your preference
 set -e
 set -o pipefail
-meld ~/workspace/github.com/beenotung/ts-liveview/$1 $1
+
+upstream_path="$HOME/workspace/github.com/beenotung/ts-liveview"
+
+if [ $# == 1 ]; then
+  compare_path="$1"
+else
+  read -p "directory/file to compare: " compare_path
+fi
+
+function clean_up {
+  rm -rf \
+    "$1/node_modules" \
+    "$1/db" \
+    "$1/build" \
+    "$1/dist" \
+}
+
+clean_up .
+clean_up "$upstream_path"
+
+meld "$upstream_path/$compare_path" "$compare_path"
