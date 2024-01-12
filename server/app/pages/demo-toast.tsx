ddfda564dcc0339eb3b5494edc074e9534688275
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-01-12 11:49:45 +0800

    chore: update demo to show alert as well

--- server/app/pages/demo-toast.tsx
+++ server/app/pages/demo-toast.tsx
@@ -1,7 +1,10 @@
 import { loadClientPlugin } from '../../client-plugin.js'
 import { title } from '../../config.js'
+import { sweetAlertAssets } from '../components/sweetalert.js'
 import { o } from '../jsx/jsx.js'
 import { Routes } from '../routes.js'
+
+let icons = ['success', 'error', 'warning', 'info', 'question']
 
 let content = (
   <div>
@@ -10,13 +13,25 @@ let content = (
       This page demo client-side plugins that bundle npm packages and run them
       in the browser.
     </p>
-    <h2>Sweet Alert Toasts</h2>
+    <h2>Sweet Alert Demo</h2>
     {loadClientPlugin({ entryFile: 'dist/client/sweetalert.js' }).node}
+    <h3>Toast</h3>
     <div style="display:flex;flex-wrap:wrap;gap:0.5rem">
       <button onclick="showToast('sample text message')">default</button>
       {[
-        ['success', 'error', 'warning', 'info', 'question'].map(icon => (
+        icons.map(icon => (
           <button onclick={`showToast('sample ${icon} message', '${icon}')`}>
+            {icon}
+          </button>
+        )),
+      ]}
+    </div>
+    <h3>Alert</h3>
+    <div style="display:flex;flex-wrap:wrap;gap:0.5rem">
+      <button onclick="showAlert('sample text message')">default</button>
+      {[
+        icons.map(icon => (
+          <button onclick={`showAlert('sample ${icon} message', '${icon}')`}>
             {icon}
           </button>
         )),
