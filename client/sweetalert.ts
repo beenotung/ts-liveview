6668e8189b25c74aff9517b44c3fea8e8cf2ed46
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-01-12 11:49:13 +0800

    chore: remove restriction on sweetalert to any countries

--- client/sweetalert.ts
+++ client/sweetalert.ts
@@ -1,4 +1,4 @@
-import Swal, { SweetAlertIcon, SweetAlertOptions } from 'sweetalert2'
+import Swal, { SweetAlertIcon, SweetAlertOptions } from 'sweetalert2-unrestricted'
 
 function showToast(title: SweetAlertOptions['title'], icon: SweetAlertIcon) {
   const Toast = Swal.mixin({
