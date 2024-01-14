3e74acfab27fe99eae43a3b21edb06a2becd3163
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-01-14 14:21:54 +0800

    patch: fix migration for verification_code and user table

---
+++ db/migrations/20240114060756_verfication_code--passcode--not-nullable.ts
@@ -0,0 +1,38 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('verification_code', table => {
+    table.dropIndex(['passcode'])
+  })
+  {
+    await knex.raw('delete from `verification_code` where `passcode` is null')
+    const rows = await knex.select('id', 'passcode').from('verification_code')
+    await knex.raw('alter table `verification_code` drop column `passcode`')
+    await knex.raw("alter table `verification_code` add column `passcode` char(6) not null")
+    for (let row of rows) {
+      await knex('verification_code').update({ passcode: row.passcode }).where({ id: row.id })
+    }
+  }
+  await knex.schema.alterTable('verification_code', table => {
+    table.index(['passcode'])
+  })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('verification_code', table => {
+    table.dropIndex(['passcode'])
+  })
+  {
+    const rows = await knex.select('id', 'passcode').from('verification_code')
+    await knex.raw('alter table `verification_code` drop column `passcode`')
+    await knex.raw("alter table `verification_code` add column `passcode` char(6) null")
+    for (let row of rows) {
+      await knex('verification_code').update({ passcode: row.passcode }).where({ id: row.id })
+    }
+  }
+  await knex.schema.alterTable('verification_code', table => {
+    table.index(['passcode'])
+  })
+}
