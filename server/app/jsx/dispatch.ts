61d07dab7c5404621e763955580ddd82cef68ab0
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-01-21 00:30:30 +0800

    chore: deduplicate error handling from dispatchUpdate() into onWsMessage()

--- server/app/jsx/dispatch.ts
+++ server/app/jsx/dispatch.ts
@@ -1,7 +1,6 @@
 import type { ServerMessage } from '../../../client/types'
 import type { VElement, title } from '../../../client/jsx/types'
 import type { WsContext } from '../context'
-import { EarlyTerminate, MessageException } from '../helpers.js'
 import type { Element, Component } from './types'
 import { nodeToVElementOptimized } from './vnode.js'
 
@@ -12,20 +11,9 @@ export function dispatchUpdate(
 ) {
   console.log('dispatch update, node:')
   console.dir(node, { depth: 1 })
-  try {
-    const vElement: VElement = nodeToVElementOptimized(node, context)
-    const message: ServerMessage = title
-      ? ['update', vElement, title]
-      : ['update', vElement]
-    context.ws.send(message)
-  } catch (error) {
-    if (error === EarlyTerminate) {
-      return
-    }
-    if (error instanceof MessageException) {
-      context.ws.send(error.message)
-      return
-    }
-    console.error('Failed to dispatch update:', error)
-  }
+  const vElement: VElement = nodeToVElementOptimized(node, context)
+  const message: ServerMessage = title
+    ? ['update', vElement, title]
+    : ['update', vElement]
+  context.ws.send(message)
 }
