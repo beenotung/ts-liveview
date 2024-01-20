555a975fec24f71e629a01add9b97d201d40bce1
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-01-20 17:28:55 +0800

    chore: use more specific message in add button

--- server/app/pages/route-template-hybrid.tsx
+++ server/app/pages/route-template-hybrid.tsx
@@ -68,7 +68,7 @@ function Main(attrs: {}, context: Context) {
             </Link>
           ) : (
             <p>
-              You can add item after <Link href="/register">register</Link>.
+              You can add __name__ after <Link href="/register">register</Link>.
             </p>
           )}
         </>
@@ -88,7 +88,7 @@ function Main(attrs: {}, context: Context) {
             </Link>
           ) : (
             <p>
-              You can add item after <Link href="/register">register</Link>.
+              You can add __name__ after <Link href="/register">register</Link>.
             </p>
           )}
         </>
