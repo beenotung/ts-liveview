223d3bdc712189fb9e5cb11517f39cf714d8ab81
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-01-21 20:19:23 +0800

    chore: remove /app prefix in routes
    chore: remove web and hybrid pages for ionic-template
    
    chore: remove unused import
    
    chore: remove unused code

--- server/app/components/back-to-link.tsx
+++ server/app/components/back-to-link.tsx
@@ -1,20 +1,10 @@
 import { o } from '../jsx/jsx.js'
-import { Content } from './page.js'
 import { Link } from './router.js'
 
 export function BackToLink(attrs: { title: string; href: string }) {
   return (
-    <Content
-      web={
-        <p>
-          Back to <Link href={attrs.href}>{attrs.title}</Link>
-        </p>
-      }
-      ionic={
-        <Link href={attrs.href} tagName="ion-button" is-back>
-          Back to {attrs.title}
-        </Link>
-      }
-    ></Content>
+    <Link href={attrs.href} tagName="ion-button" is-back>
+      Back to {attrs.title}
+    </Link>
   )
 }
