2b23eda76f295f9f67f2a1a8d264b35cbaa2ee51
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-01-21 20:19:23 +0800

    chore: remove /app prefix in routes

--- server/app/pages/app-character.tsx
+++ server/app/pages/app-character.tsx
@@ -71,7 +71,7 @@ function DetailPage(attrs: { item: Character }, context: DynamicContext) {
       <ion-header>
         <ion-toolbar role="heading" aria-level="1">
           <IonBackButton
-            href="/app/home"
+            href="/"
             buttonsSlot="start"
             color="primary"
             backText="List"
@@ -97,7 +97,7 @@ function DetailPage(attrs: { item: Character }, context: DynamicContext) {
 }
 
 let routes: Routes = {
-  '/app/characters/:id': {
+  '/characters/:id': {
     resolve(context) {
       let id = context.routerMatch?.params.id
       let item = characters.find(item => item.id == id)
