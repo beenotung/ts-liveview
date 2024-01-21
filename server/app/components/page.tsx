7d9259a3548e8c004761e467793acaef3023e372
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-01-21 20:19:23 +0800

    chore: remove /app prefix in routes
    chore: remove web and hybrid pages for ionic-template
    
    chore: remove unused import
    
    chore: remove unused code

--- server/app/components/page.tsx
+++ server/app/components/page.tsx
@@ -1,4 +1,4 @@
-import { config, LayoutType } from '../../config.js'
+import { config } from '../../config.js'
 import { o } from '../jsx/jsx.js'
 import { Node, NodeList } from '../jsx/types.js'
 import { IonBackButton } from './ion-back-button.js'
@@ -59,26 +59,4 @@ function IonicPage(attrs: PageAttrs) {
   )
 }
 
-function WebPage(attrs: PageAttrs) {
-  return (
-    <div id={attrs.id} class={attrs.class} style={attrs.style}>
-      {attrs.title ? <h1>{attrs.title}</h1> : null}
-      {attrs.children ? [attrs.children] : null}
-    </div>
-  )
-}
-
-export let is_ionic = config.layout_type === LayoutType.ionic
-
-export let Page = is_ionic ? IonicPage : WebPage
-
-type Content = Node | (() => Node)
-
-export function Content(attrs: { ionic?: Content; web?: Content }): Node {
-  let content =
-    config.layout_type === LayoutType.ionic ? attrs.ionic : attrs.web
-  if (typeof content === 'function') {
-    return content()
-  }
-  return content
-}
+export let Page = IonicPage
