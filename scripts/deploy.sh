ac58699ab44e50537e77901f820a5692ca2c9ca3
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-02-01 00:41:01 +0800

    feat: ask deploy mode from cli if the MODE variable is not specified

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,26 @@ set -o pipefail
 
 source scripts/config
 
+if [ -z "$MODE" ]; then
+  echo "possible mode:"
+  echo "  [f] first   (start new pm2 process)"
+  echo "  [q] quick   (reload without database migration)"
+  echo "  [ ] default (reload with database migration)"
+  read -p "mode: " MODE
+fi
+case "$MODE" in
+  f)
+    MODE="first"
+    ;;
+  q)
+    MODE="quick"
+    ;;
+  '')
+    MODE="default"
+    ;;
+esac
+echo "deploy mode: $MODE"
+
 set -x
 
 if [ "$MODE" == "quick" ]; then
