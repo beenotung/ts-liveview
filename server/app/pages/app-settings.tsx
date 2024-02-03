f657bed4d7673516d8cfd79d4b447846062fc867
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-02-04 00:04:50 +0800

    chore: remove unused parts in app-settings demo chore: rename AppSettings to Settings

--- server/app/pages/app-settings.tsx
+++ server/app/pages/app-settings.tsx
@@ -10,10 +10,9 @@ import { Link, Redirect } from '../components/router.js'
 import { renderError } from '../components/error.js'
 
 let pageTitle = 'App Settings'
-let addPageTitle = 'Add App Settings'
 
 let style = Style(/* css */ `
-#AppSettings {
+#Settings {
 
 }
 `)
@@ -29,7 +28,7 @@ let page = (
         </ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content id="AppSettings" class="ion-padding">
+    <ion-content id="Settings" class="ion-padding">
       Items
       <Main />
     </ion-content>
@@ -51,125 +50,6 @@ function Main(attrs: {}, context: Context) {
           </ion-item>
         ))}
       </ion-list>
-      <Link href="/app-settings/add" tagName="ion-button">
-        {addPageTitle}
-      </Link>
-    </>
-  )
-}
-
-let addPage = (
-  <>
-    {Style(/* css */ `
-#AddAppSettings .hint {
-  margin-inline-start: 1rem;
-  margin-block: 0.25rem;
-}
-`)}
-    <ion-header>
-      <ion-toolbar>
-        <IonBackButton href="/app-settings" backText={pageTitle} />
-        <ion-title role="heading" aria-level="1">
-          {addPageTitle}
-        </ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content id="AddAppSettings" class="ion-padding">
-      <form
-        method="POST"
-        action="/app-settings/add/submit"
-        onsubmit="emitForm(event)"
-      >
-        <ion-list>
-          <ion-item>
-            <ion-input
-              name="title"
-              label="Title*:"
-              label-placement="floating"
-              required
-              minlength="3"
-              maxlength="50"
-            />
-          </ion-item>
-          <p class="hint">(3-50 characters)</p>
-          <ion-item>
-            <ion-input
-              name="slug"
-              label="Slug*: (unique url)"
-              label-placement="floating"
-              required
-              pattern="(\w|-|\.){1,32}"
-            />
-          </ion-item>
-          <p class="hint">
-            (1-32 characters of: <code>a-z A-Z 0-9 - _ .</code>)
-          </p>
-        </ion-list>
-        <div style="margin-inline-start: 1rem">
-          <ion-button type="submit">Submit</ion-button>
-        </div>
-        <p>
-          Remark:
-          <br />
-          *: mandatory fields
-        </p>
-      </form>
-    </ion-content>
-  </>
-)
-
-let submitParser = object({
-  title: string({ minLength: 3, maxLength: 50 }),
-  slug: string({ match: /^[\w-]{1,32}$/ }),
-})
-
-function Submit(attrs: {}, context: DynamicContext) {
-  try {
-    let body = getContextFormBody(context)
-    let input = submitParser.parse(body)
-    let id = items.push({
-      title: input.title,
-      slug: input.slug,
-    })
-    return <Redirect href={`/app-settings/result?id=${id}`} />
-  } catch (error) {
-    return (
-      <Redirect
-        href={
-          '/app-settings/result?' +
-          new URLSearchParams({ error: String(error) })
-        }
-      />
-    )
-  }
-}
-
-function SubmitResult(attrs: {}, context: DynamicContext) {
-  let params = new URLSearchParams(context.routerMatch?.search)
-  let error = params.get('error')
-  let id = params.get('id')
-  return (
-    <>
-      <ion-header>
-        <ion-toolbar>
-          <IonBackButton href="/app-settings/add" backText="Form" />
-          <ion-title role="heading" aria-level="1">
-            Submitted {pageTitle}
-          </ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <ion-content id="AddAppSettings" class="ion-padding">
-        {error ? (
-          renderError(error, context)
-        ) : (
-          <>
-            <p>Your submission is received (#{id}).</p>
-            <Link href="/app-settings" tagName="ion-button">
-              Back to {pageTitle}
-            </Link>
-          </>
-        )}
-      </ion-content>
     </>
   )
 }
