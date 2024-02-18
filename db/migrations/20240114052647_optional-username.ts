3898475f5abd1d98e3df140356b651b318e63ff5
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-02-18 15:56:50 +0800

    chore: format with prettier

--- db/migrations/20240114052647_optional-username.ts
+++ db/migrations/20240114052647_optional-username.ts
@@ -1,30 +1,36 @@
-import { Knex } from "knex";
-
+import { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
   {
     const rows = await knex.select('id', 'username').from('user')
-    await knex.schema.alterTable('user', table => table.dropUnique(['username']))
+    await knex.schema.alterTable('user', table =>
+      table.dropUnique(['username']),
+    )
     await knex.raw('alter table `user` drop column `username`')
-    await knex.raw("alter table `user` add column `username` varchar(32) null")
+    await knex.raw('alter table `user` add column `username` varchar(32) null')
     await knex.schema.alterTable('user', table => table.unique(['username']))
     for (let row of rows) {
-      await knex('user').update({ username: row.username }).where({ id: row.id })
+      await knex('user')
+        .update({ username: row.username })
+        .where({ id: row.id })
     }
   }
 }
-
 
 export async function down(knex: Knex): Promise<void> {
   {
     await knex.raw('delete from `user` where `username` is null')
     const rows = await knex.select('id', 'username').from('user')
-    await knex.schema.alterTable('user', table => table.dropUnique(['username']))
+    await knex.schema.alterTable('user', table =>
+      table.dropUnique(['username']),
+    )
     await knex.raw('alter table `user` drop column `username`')
-    await knex.raw("alter table `user` add column `username` text not null")
+    await knex.raw('alter table `user` add column `username` text not null')
     await knex.schema.alterTable('user', table => table.unique(['username']))
     for (let row of rows) {
-      await knex('user').update({ username: row.username }).where({ id: row.id })
+      await knex('user')
+        .update({ username: row.username })
+        .where({ id: row.id })
     }
   }
 }
