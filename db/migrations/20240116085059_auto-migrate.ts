0e31067e10b578089ac8824d71b888bb39145f22
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-02-18 15:56:50 +0800

    chore: format with prettier

--- db/migrations/20240116085059_auto-migrate.ts
+++ db/migrations/20240116085059_auto-migrate.ts
@@ -1,11 +1,19 @@
-import { Knex } from "knex";
-
+import { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
-  let attempts = await knex.select('id', 'match_id').from('verification_attempt').whereNotNull('match_id')
-  await knex.schema.alterTable('verification_attempt', table => table.dropColumn('match_id'))
-  await knex.raw('alter table `verification_code` add column `match_id` integer null references `verification_attempt`(`id`)')
-  await knex.raw('alter table `verification_code` add column `user_id` integer null references `user`(`id`)')
+  let attempts = await knex
+    .select('id', 'match_id')
+    .from('verification_attempt')
+    .whereNotNull('match_id')
+  await knex.schema.alterTable('verification_attempt', table =>
+    table.dropColumn('match_id'),
+  )
+  await knex.raw(
+    'alter table `verification_code` add column `match_id` integer null references `verification_attempt`(`id`)',
+  )
+  await knex.raw(
+    'alter table `verification_code` add column `user_id` integer null references `user`(`id`)',
+  )
   for (let attempt of attempts) {
     await knex('verification_code')
       .update({ match_id: attempt.id })
@@ -18,13 +26,14 @@ export async function up(knex: Knex): Promise<void> {
   `)
 }
 
-
 export async function down(knex: Knex): Promise<void> {
   let attempts = await knex.select('id', 'match_id').from('verification_code')
   await knex.raw('alter table `verification_code` drop column `user_id`')
   await knex.raw('alter table `verification_code` drop column `match_id`')
-  await knex.raw('alter table `verification_attempt` add column `match_id` integer null references `verification_code`(`id`)')
-  for (let attempt of attempts){
+  await knex.raw(
+    'alter table `verification_attempt` add column `match_id` integer null references `verification_code`(`id`)',
+  )
+  for (let attempt of attempts) {
     await knex('verification_attempt')
       .update({ match_id: attempt.id })
       .where({ id: attempt.match_id })
