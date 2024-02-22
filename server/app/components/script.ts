5231e5c50b210cfb75a82f97f412d08a5f35c773
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-02-22 17:31:45 +0800

    feat: add ScriptFlag to allow skipping minificaiton

--- server/app/components/script.ts
+++ server/app/components/script.ts
@@ -1,45 +1,74 @@
 import { config } from '../../config.js'
-import type { Element, Raw } from '../jsx/types'
+import type { Raw } from '../jsx/types'
 import * as esbuild from 'esbuild'
 
 const cache = new Map<string, string>()
 
-export function Script(js: string): Element {
-  if (config.production) {
-    if (cache.has(js)) {
-      js = cache.get(js) as string
-    } else {
-      let code = esbuild.transformSync(js, {
-        minify: true,
-        loader: 'js',
-        target: config.client_target,
-      }).code
-      cache.set(js, code)
-      js = code
-    }
+/**
+ * @param js javascript code without script tag
+ * @returns minified javascript code
+ * @description memorized (cached)
+ */
+function minify(js: string): string {
+  let code = cache.get(js)
+  if (typeof code === 'string') {
+    return code
   }
-  const raw: Raw = ['raw', js]
-  return ['script', undefined, [raw]]
+  code = esbuild.transformSync(js, {
+    minify: true,
+    loader: 'js',
+    target: config.client_target,
+  }).code
+
+  cache.set(js, code)
+  return code
+}
+
+/**
+ * @default 'no-minify' when not in production
+ * @default 'minify' when in production
+ */
+export type ScriptFlag = 'no-minify' | 'minify'
+
+/**
+ * @description For static script.
+ * Minimize in production mode and memorized (cached).
+ * @returns script element
+ */
+export function Script(js: string, flag?: ScriptFlag): Raw {
+  if (flag == 'minify' || (flag != 'no-minify' && config.production)) {
+    js = minify(js)
+  }
+  return ['raw', `<script>${js}</script>`]
 }
 
 /**
  * @description use iife (Immediately Invoked Function Expression) to avoid name clash with other parts of the page.
  * */
-export function iife<F extends () => void>(fn: F): Element
+export function iife<F extends () => void>(fn: F, flag?: ScriptFlag): Raw
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function iife<F extends (...args: any[]) => void>(
   fn: F,
   args: Parameters<F>,
-): Element
+  flag?: ScriptFlag,
+): Raw
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function iife<F extends (...args: any[]) => void>(
   fn: F,
-  args?: Parameters<F>,
-): Element {
-  if (args && args.length > 0) {
-    return Script(`(${fn}).apply(this,${JSON.stringify(args)})`)
+  args?: Parameters<F> | ScriptFlag,
+  flag?: ScriptFlag,
+): Raw {
+  args
+  if (typeof args == 'string') {
+    flag = args
+    args = undefined
   }
-  return Script(`(${fn})()`)
+  if (args && args.length > 0) {
+    let args_code = JSON.stringify(args)
+    args_code = args_code.slice(1, args_code.length - 1)
+    return Script(`(${fn})(${args_code})`, flag)
+  }
+  return Script(`(${fn})()`, flag)
 }
 
 /**
