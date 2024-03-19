cfe6ed5fe236cd9f69877630d10b7051586a23cd
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-03-19 17:48:51 +0800

    feat: support mocking in sendEmail()
    # Conflicts:
    #	server/email.ts

--- server/email.ts
+++ server/email.ts
@@ -1,5 +1,9 @@
 import nodemailer, { SendMailOptions, createTransport } from 'nodemailer'
+import { debugLog } from './debug.js'
 import { env } from './env.js'
+
+let log = debugLog('email.ts')
+log.enabled = true
 
 let transport = createTransport({
   service: env.EMAIL_SERVICE,
@@ -19,7 +23,22 @@ export interface SentMessageInfo {
   messageId: string
 }
 
-export async function sendEmail(options: SendMailOptions) {
+export async function sendEmail(
+  options: SendMailOptions,
+): Promise<SentMessageInfo> {
+  if (env.EMAIL_USER === 'skip') {
+    log('sendEmail:', options)
+    let to: string[] = Array.isArray(options.to)
+      ? (options.to as string[])
+      : [options.to as string]
+    return {
+      accepted: to,
+      rejected: [],
+      response: '250 2.0.0 OK',
+      envelope: { from: options.from as string, to },
+      messageId: 'mock-message-id',
+    }
+  }
   let info = await transport.sendMail(options)
   return info as SentMessageInfo
 }
