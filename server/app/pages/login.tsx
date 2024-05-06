a3da5095cca3a02913e25cc5a6996ed5d8df35b6
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-05-06 19:21:14 +0800

    chore: submit login/register form with ws to allow showing password in alert in dev mode

--- server/app/pages/login.tsx
+++ server/app/pages/login.tsx
@@ -166,7 +166,7 @@ let guestView = (
     <form
       method="POST"
       action="/verify/email/submit"
-      // onsubmit="emitForm(event)"
+      onsubmit="emitForm(event)"
     >
       {emailFormBody}
     </form>
