90cbfe938358df6ca457380912144fe0158441bc
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-06-07 14:21:32 +0800

    feat: add toUrl() to compose a url without routes dict

--- server/app/helpers.ts
+++ server/app/helpers.ts
@@ -67,7 +67,21 @@ export function toRouteUrl<R extends object, K extends string & keyof R>(
     /** @description to apply `JSON.stringify()` on the result if enabled */
     json?: boolean
   },
-) {
+): string {
+  return toUrl<K>(key, options)
+}
+
+export function toUrl<K extends string>(
+  /** @description the url template */
+  key: K,
+  /** @description the variables in url */
+  options?: {
+    params?: RouteParameters<K>
+    query?: object
+    /** @description to apply `JSON.stringify()` on the result if enabled */
+    json?: boolean
+  },
+): string {
   let params = options?.params as Record<string, string | number>
   let url = key as string
   for (let part of url.split('/')) {
