bdedefb66dcb2ff101daedc2782801537da3df65
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-06-11 19:38:29 +0800

    feat: add Button component

---
+++ server/app/components/button.tsx
@@ -0,0 +1,14 @@
+import { NodeList } from '../jsx/types.js'
+
+export function Button(attrs: { url: string; children?: NodeList } & object) {
+  let { url, children, ...rest } = attrs
+  return [
+    'button',
+    {
+      'data-url': url,
+      'onclick': 'emit(this.dataset.url)',
+      ...rest,
+    },
+    children,
+  ]
+}
