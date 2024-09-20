06222390d7a91972c9a154fefa3dda399a7f4c7d
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-09-20 19:15:48 +0800

    chore: make both en and zh version accessible by search engine

--- server/app/components/ui-language.tsx
+++ server/app/components/ui-language.tsx
@@ -1,22 +1,77 @@
-import { Context, getContextLanguage } from '../context.js'
+import { Context, DynamicContext, getContextLanguage } from '../context.js'
 import { o } from '../jsx/jsx.js'
+import { ResolvedPageRoute, Routes, getContextSearchParams } from '../routes.js'
 import { Raw } from './raw.js'
+import { apiEndpointTitle } from '../../config.js'
+import { toRouteUrl } from '../../url.js'
+import { Redirect } from './router.js'
 
 export function PickLanguage(attrs: { style?: string }, context: Context) {
   let lang = getContextLanguage(context)
+  let return_url = context.type == 'static' ? '' : context.url
   return (
     <div style={attrs.style}>
       {lang === 'zh' ? 'Language' : '界面語言'}:{' '}
-      <button onclick="switchLang('zh')">繁體中文</button> |{' '}
-      <button onclick="switchLang('en')">English</button>
+      <a
+        onclick="switchLang(event, 'zh')"
+        href={toRouteUrl(routes, '/set-lang/:lang', {
+          params: { lang: 'zh' },
+          query: { return_url },
+        })}
+      >
+        <button>繁體中文</button>
+      </a>{' '}
+      |{' '}
+      <a
+        onclick="switchLang(event, 'en')"
+        href={toRouteUrl(routes, '/set-lang/:lang', {
+          params: { lang: 'en' },
+          query: { return_url },
+        })}
+      >
+        <button>English</button>
+      </a>
       {Raw(/* html */ `
 <script>
-function switchLang(lang){
+function switchLang(event, lang){
   document.cookie = 'lang=' + lang + ';SameSite=Lax;path=/'
   remount()
+  event.preventDefault()
 }
 </script>
 `)}
     </div>
   )
 }
+
+function submit(context: DynamicContext): ResolvedPageRoute {
+  let lang = context.routerMatch?.params.lang
+  let return_url = getContextSearchParams(context).get('return_url')
+  if (context.type == 'express') {
+    context.res.cookie('lang', lang, {
+      sameSite: 'lax',
+      path: '/',
+    })
+  } else {
+    let cookie = 'lang=' + lang + ';SameSite=Lax;path=/'
+    context.ws.send(['set-cookie', cookie])
+  }
+  return {
+    title: apiEndpointTitle,
+    description: 'set the locale language',
+    node: return_url ? (
+      <Redirect href={return_url} />
+    ) : (
+      <p>Updated language preference.</p>
+    ),
+  }
+}
+
+let routes = {
+  '/set-lang/:lang': {
+    streaming: false,
+    resolve: submit,
+  },
+} satisfies Routes
+
+export default { routes }
