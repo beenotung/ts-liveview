ce8084bba2164ca6eafd434228f4d5cc80b94ef7
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-10-30 14:03:43 +0800

    feat: add helper function throwIfInAPI()

--- server/app/pages/route-template-web.tsx
+++ server/app/pages/route-template-web.tsx
@@ -2,18 +2,12 @@ import { o } from '../jsx/jsx.js'
 import { Routes } from '../routes.js'
 import { apiEndpointTitle, title } from '../../config.js'
 import Style from '../components/style.js'
-import {
-  Context,
-  DynamicContext,
-  getContextFormBody,
-  isAjax,
-} from '../context.js'
+import { Context, DynamicContext, getContextFormBody } from '../context.js'
 import { mapArray } from '../components/fragment.js'
 import { object, string } from 'cast.ts'
 import { Link, Redirect } from '../components/router.js'
 import { renderError } from '../components/error.js'
-import { ServerMessage } from '../../../client/types.js'
-import { EarlyTerminate, MessageException } from '../../exception.js'
+import { throwIfInAPI } from '../../exception.js'
 
 let pageTitle = '__title__'
 let addPageTitle = 'Add __title__'
@@ -120,24 +114,7 @@ function Submit(attrs: {}, context: DynamicContext) {
     })
     return <Redirect href={`/__url__/result?id=${id}`} />
   } catch (error) {
-    let message: ServerMessage =
-      error instanceof MessageException
-        ? error.message
-        : [
-            'batch',
-            [
-              ['update-text', '#add-message', String(error)],
-              ['add-class', '#add-message', 'error'],
-            ],
-          ]
-    if (context.type == 'ws') {
-      context.ws.send(message)
-      throw EarlyTerminate
-    }
-    if (context.type == 'express' && isAjax(context)) {
-      context.res.json({ message })
-      throw EarlyTerminate
-    }
+    throwIfInAPI(error, '#add-message', context)
     return (
       <Redirect
         href={
--- server/exception.ts
+++ server/exception.ts
@@ -1,4 +1,5 @@
 import type { ServerMessage } from '../client/types'
+import { isAjax, Context } from './app/context.js'
 import type { Node } from './app/jsx/types'
 
 export class HttpError extends Error {
@@ -12,6 +13,31 @@ export class HttpError extends Error {
 
 export class MessageException {
   constructor(public message: ServerMessage) {}
+}
+
+export function throwIfInAPI(
+  error: unknown,
+  selector: string,
+  context: Context,
+) {
+  let message: ServerMessage =
+    error instanceof MessageException
+      ? error.message
+      : [
+          'batch',
+          [
+            ['update-text', selector, String(error)],
+            ['add-class', selector, 'error'],
+          ],
+        ]
+  if (context.type == 'ws') {
+    context.ws.send(message)
+    throw EarlyTerminate
+  }
+  if (context.type == 'express' && isAjax(context)) {
+    context.res.json({ message })
+    throw EarlyTerminate
+  }
 }
 
 export class ErrorNode {
