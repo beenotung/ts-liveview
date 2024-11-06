fa342a2d8b3a72a3b69fb2cb37f4ee695ce23908
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-11-06 22:37:06 +0800

    chore: switch to use argon2 instead of bcrypt

--- server/hash.ts
+++ server/hash.ts
@@ -1,53 +1,17 @@
 import { createHash } from 'crypto'
-import { compare, hash, hashSync } from 'bcrypt'
-import { createHash } from 'crypto'
+import * as argon2 from 'argon2'
 
 export function hashText(text: string) {
   return createHash('sha256').update(text).digest('hex')
 }
 
-// reference: https://stackoverflow.com/a/61304956/3156509
-function calcCost(password: string) {
-  const InitialCost = 6
-  const TargetTime = 250
-  const MinCost = 10
-  const MaxCost = 31
-
-  let cost = InitialCost
-  let start = Date.now()
-  hashSync(password, cost)
-  let end = Date.now()
-  let time = end - start
-  while (time < TargetTime) {
-    cost += 1
-    time *= 2
-  }
-
-  return cost < MinCost ? MinCost : cost > MaxCost ? MaxCost : cost
-}
-
-const max_bcrypt_input_length = 72
-
-function preprocessPassword(password: string): string {
-  let byte_length = Buffer.from(password).length
-  if (byte_length <= max_bcrypt_input_length) {
-    return password
-  }
-  let hash = createHash('sha256')
-  hash.write(password)
-  return hash.digest('hex')
-}
-
 export async function hashPassword(password: string): Promise<string> {
-  password = preprocessPassword(password)
-  let cost = calcCost(password)
-  return hash(password, cost)
+  return argon2.hash(password)
 }
 
 export function comparePassword(options: {
   password: string
   password_hash: string
 }): Promise<boolean> {
-  let password = preprocessPassword(options.password)
-  return compare(password, options.password_hash)
+  return argon2.verify(options.password_hash, options.password)
 }
