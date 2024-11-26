a7125e107f85e2e7fa226bbe6967475de526be34
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-11-26 17:40:05 +0800

    ui: support ionic mode for status-page

--- server/app/components/status-page.tsx
+++ server/app/components/status-page.tsx
@@ -1,6 +1,8 @@
+import { config, LayoutType } from '../../config.js'
 import { Context, getContextUrl } from '../context.js'
 import { o } from '../jsx/jsx.js'
 import { Node } from '../jsx/types.js'
+import { IonBackButton } from './ion-back-button.js'
 import { LocaleVariants, isPreferZh } from './locale.js'
 import SourceCode from './source-code.js'
 
@@ -10,19 +12,43 @@ function StatusPage(
     id: string
     title: LocaleVariants
     page?: string
+    backText?: string
+    backHref?: string
   },
   context: Context,
 ): Node {
+  let backText = attrs.backText || 'Home'
+  let backHref = attrs.backHref || '/'
   if (context.type === 'express' && !context.res.headersSent) {
     context.res.status(attrs.status)
   }
   let zh = isPreferZh(context)
+  let title = zh ? attrs.title.zh : attrs.title.en
+  let label = zh ? '網址' : 'Url'
+  if (config.layout_type === LayoutType.ionic) {
+    return (
+      <>
+        <ion-header>
+          <ion-toolbar>
+            <IonBackButton href={backHref} backText={backText} />
+            <ion-title role="heading" aria-level="1">
+              {title}
+            </ion-title>
+          </ion-toolbar>
+        </ion-header>
+        <ion-content id={attrs.id} class="ion-padding">
+          <p>
+            {label}: <code>{getContextUrl(context)}</code>
+          </p>
+        </ion-content>
+      </>
+    )
+  }
   return (
     <div id={attrs.id}>
-      <h1>{zh ? attrs.title.zh : attrs.title.en}</h1>
+      <h1>{title}</h1>
       <p>
-        {zh ? '網址: ' : 'Url: '}
-        <code>{getContextUrl(context)}</code>
+        {label}: <code>{getContextUrl(context)}</code>
       </p>
       {attrs.page ? <SourceCode page={attrs.page} /> : null}
     </div>
