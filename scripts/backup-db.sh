3b49ac6a52c90d48df279d744ee1daf69c456081
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-12-11 03:26:40 +0800

    chore: update backup-db.sh to include flow to replace local db

--- scripts/backup-db.sh
+++ scripts/backup-db.sh
@@ -6,10 +6,19 @@ source scripts/config
 
 ssh "$user@$host" "
   set -e
-  cd $root_dir
-	sqlite3 data/db.sqlite3 '.backup data/backup.sqlite3'
+  cd $root_dir/data
+  sqlite3 db.sqlite3 '.backup backup.sqlite3'
 "
 
 rsync -SavlPz "$user@$host:$root_dir/data/backup.sqlite3" "data/"
 
-ls -lh data/backup.sqlite3
+cd data
+ls -lh backup.sqlite3
+
+read -p "Replace local db? (y/N): " ans
+if [ "$ans" = "y" ]; then
+  echo "Replacing local db..."
+  sqlite3 backup.sqlite3 '.backup db.sqlite3'
+else
+  echo "Skipping local db replacement."
+fi
