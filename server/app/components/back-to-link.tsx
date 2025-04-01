7568bc2caf3c23a9eac471c0e57c9812e0f0f278
Beeno Tung <aabbcc1241@yahoo.com.hk>
2025-04-02 07:41:02 +0800

    feat: add <GoToLink/>

--- server/app/components/back-to-link.tsx
+++ server/app/components/back-to-link.tsx
@@ -19,3 +19,20 @@ export function BackToLink(attrs: { title: string; href: string }) {
     ></Content>
   )
 }
+
+export function GoToLink(attrs: { title: string; href: string }) {
+  return (
+    <Content
+      web={
+        <p>
+          <Locale en="Go to" zh_hk="前往" zh_cn="前往" /> {attrs.title}
+        </p>
+      }
+      ionic={
+        <Link href={attrs.href} tagName="ion-button">
+          <Locale en="Go to" zh_hk="前往" zh_cn="前往" /> {attrs.title}
+        </Link>
+      }
+    ></Content>
+  )
+}
