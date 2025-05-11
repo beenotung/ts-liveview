fbb46405f03a6249525d7d28e64e212307afdc72
Beeno Tung <aabbcc1241@yahoo.com.hk>
2025-05-11 08:43:58 +0800

    ui: fallback to ionic mode even for web version

---
+++ server/app/styles/ionic-style.tsx
@@ -0,0 +1,12 @@
+import { o } from '../jsx/jsx.js'
+
+export let ionicStyle = (
+  <>
+    <script
+      type="module"
+      src="/npm/@ionic/core/dist/ionic/ionic.esm.js"
+    ></script>
+    <script nomodule src="/npm/@ionic/core/dist/ionic/ionic.js"></script>
+    <link rel="stylesheet" href="/npm/@ionic/core/css/ionic.bundle.css" />
+  </>
+)
