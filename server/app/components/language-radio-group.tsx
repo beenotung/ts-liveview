59cfe177f593fd52bb93e38256995da8cfddc9e6
Beeno Tung <aabbcc1241@yahoo.com.hk>
2025-06-28 16:03:02 +0800

    patch: set lang cookie in client side when choosing language in language-radio-group

--- server/app/components/language-radio-group.tsx
+++ server/app/components/language-radio-group.tsx
@@ -1,12 +1,14 @@
 import { Context, getContextLanguage } from '../context.js'
 import { o } from '../jsx/jsx.js'
 import { Script } from './script.js'
+import { language_max_age } from './ui-language.js'
 
 let script = Script(/* javascript */ `
 function initLanguageRadioGroup(){
   let radioGroup = document.getElementById('languageRadioGroup')
   radioGroup.addEventListener('ionChange', async (event) => {
     let lang = event.detail.value
+    document.cookie = 'lang=' + lang + ';SameSite=Lax;path=/;max-age=${language_max_age}'
     let url = '/set-lang/:lang'.replace(':lang', lang)
     let return_url = window.location.href.replace(location.origin, '')
     emit(url, return_url)
