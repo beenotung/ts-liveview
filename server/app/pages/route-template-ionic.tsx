d5e0c09cc497c0abcd4e1366f5076c0aa2b051e5
Beeno Tung <aabbcc1241@yahoo.com.hk>
2025-07-04 10:09:52 +0800

    i18n: add locale error message when submit

--- server/app/pages/route-template-ionic.tsx
+++ server/app/pages/route-template-ionic.tsx
@@ -13,7 +13,7 @@ import { IonBackButton } from '../components/ion-back-button.js'
 import { object, string } from 'cast.ts'
 import { Link, Redirect } from '../components/router.js'
 import { renderError } from '../components/error.js'
-import { Locale, Title } from '../components/locale.js'
+import { Locale, makeThrows, Title } from '../components/locale.js'
 import { proxy } from '../../../db/proxy.js'
 import { env } from '../../env.js'
 import { Script } from '../components/script.js'
@@ -204,8 +204,14 @@ let submitParser = object({
 
 function Submit(attrs: {}, context: DynamicContext) {
   try {
+    let throws = makeThrows(context)
     let user = getAuthUser(context)
-    if (!user) throw 'You must be logged in to submit ' + pageTitle
+    if (!user)
+      throws({
+        en: 'You must be logged in to submit ' + Locale(pageTitle, context),
+        zh_hk: '您必須登入才能提交 ' + Locale(pageTitle, context),
+        zh_cn: '您必須登入才能提交 ' + Locale(pageTitle, context),
+      })
     let body = getContextFormBody(context)
     let input = submitParser.parse(body)
     let id = items.push({
