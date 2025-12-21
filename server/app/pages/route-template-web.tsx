fb22dc8a636450ab8916a0c1d62d2bf0919ed117
Beeno Tung <aabbcc1241@yahoo.com.hk>
2025-12-22 06:18:06 +0800

    feat: support multiple templates in new-route.sh

--- server/app/pages/route-template-web.tsx
+++ server/app/pages/route-template-web.tsx
@@ -22,6 +22,7 @@ import Script from '../components/script.js'
 import { toSlug } from '../format/slug.js'
 import { BackToLink } from '../components/back-to-link.js'
 import { sweetAlertPlugin } from '../../client-plugins.js'
+import { getAuthUser } from '../auth/user.js'
 
 let pageTitle = <Locale en="__title__" zh_hk="__title__" zh_cn="__title__" />
 let addPageTitle = (
@@ -45,6 +46,7 @@ let items = [
 ]
 
 function ListPage(attrs: {}, context: Context) {
+  let user = getAuthUser(context)
   return (
     <>
       {style}
@@ -59,9 +61,15 @@ function ListPage(attrs: {}, context: Context) {
             </li>
           ))}
         </ul>
-        <Link href="/__url__/add">
-          <button>{addPageTitle}</button>
-        </Link>
+        {user ? (
+          <Link href="/__url__/add">
+            <button>{addPageTitle}</button>
+          </Link>
+        ) : (
+          <p>
+            You can add __name__ after <Link href="/register">register</Link>.
+          </p>
+        )}
       </div>
       {script}
     </>
@@ -166,6 +174,12 @@ let addPage = (
   </>
 )
 
+function AddPage(attrs: {}, context: DynamicContext) {
+  let user = getAuthUser(context)
+  if (!user) return <Redirect href="/login" />
+  return addPage
+}
+
 let submitParser = object({
   title: string({ minLength: 3, maxLength: 50 }),
   slug: string({ match: /^[\w\-.]{1,32}$/, case: 'lower' }),
@@ -173,6 +187,8 @@ let submitParser = object({
 
 function Submit(attrs: {}, context: DynamicContext) {
   try {
+    let user = getAuthUser(context)
+    if (!user) throw 'You must be logged in to submit ' + pageTitle
     let body = getContextFormBody(context)
     let input = submitParser.parse(body)
     let id = items.push({
@@ -414,7 +430,7 @@ let routes = {
   '/__url__/add': {
     title: <Title t={addPageTitle} />,
     description: 'TODO',
-    node: addPage,
+    node: <AddPage />,
     streaming: false,
   },
   '/__url__/add/submit': {
