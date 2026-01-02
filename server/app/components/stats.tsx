528bb1a9e2e5ba11d05be0748cea804dcc63571f
Beeno Tung <aabbcc1241@yahoo.com.hk>
2026-01-03 07:15:43 +0800

    ui: show stats counts in locale format

--- server/app/components/stats.tsx
+++ server/app/components/stats.tsx
@@ -4,7 +4,7 @@ import { o } from '../jsx/jsx.js'
 import { onWsSessionClose, sessions } from '../session.js'
 import { existsSync, renameSync } from 'fs'
 import { join } from 'path'
-import type { Context } from '../context'
+import { getContextLanguage, type Context } from '../context.js'
 import { loadNumber, saveNumber } from '../data/version-number.js'
 
 function sendMessage(message: ServerMessage, skip?: ManagedWebsocket) {
@@ -33,11 +33,15 @@ let state = {
 }
 
 export function Stats(attrs: { hidden?: boolean }, context: Context) {
+  let locales = getContextLanguage(context) || 'en-US'
+  function s(count: number) {
+    return count.toLocaleString(locales)
+  }
   let messages: ServerMessage[] = []
   if (context.type === 'express') {
     state.visit++
     saveNumber(visitFile, state.visit)
-    messages.push(['update-text', '#stats .visit', state.visit])
+    messages.push(['update-text', '#stats .visit', s(state.visit)])
   }
   let ws: ManagedWebsocket | undefined
   if (context.type === 'ws') {
@@ -48,8 +52,8 @@ export function Stats(attrs: { hidden?: boolean }, context: Context) {
       state.live.add(ws)
     }
     messages.push(
-      ['update-text', '#stats .session', state.session],
-      ['update-text', '#stats .live', state.live.size],
+      ['update-text', '#stats .session', s(state.session)],
+      ['update-text', '#stats .live', s(state.live.size)],
     )
     onWsSessionClose(ws, session => {
       let ws = session.ws
@@ -57,7 +61,7 @@ export function Stats(attrs: { hidden?: boolean }, context: Context) {
       let message: ServerMessage = [
         'update-text',
         '#stats .live',
-        state.live.size,
+        s(state.live.size),
       ]
       sendMessage(message)
     })
@@ -73,15 +77,15 @@ export function Stats(attrs: { hidden?: boolean }, context: Context) {
         <tbody>
           <tr>
             <td>#visit</td>
-            <td class="visit">{state.visit}</td>
+            <td class="visit">{s(state.visit)}</td>
           </tr>
           <tr>
             <td>#session</td>
-            <td class="session">{state.session}</td>
+            <td class="session">{s(state.session)}</td>
           </tr>
           <tr>
             <td>#live-session</td>
-            <td class="live">{state.live.size}</td>
+            <td class="live">{s(state.live.size)}</td>
           </tr>
         </tbody>
       </table>
