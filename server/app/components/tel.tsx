5c984a59df592bcc896b1fb98671ed34bc84feb1
Beeno Tung <aabbcc1241@yahoo.com.hk>
2026-01-04 14:00:30 +0800

    chore: support international format in tel.tsx

--- server/app/components/tel.tsx
+++ server/app/components/tel.tsx
@@ -1,12 +1,17 @@
+import {
+  format_mobile_phone,
+  to_full_mobile_phone,
+} from '@beenotung/tslib/tel.js'
 import { o } from '../jsx/jsx.js'
 
-export function Tel(tel: string): string {
-  tel = tel.replace('+852', '')
-  return <a href={'tel:' + tel}>{tel.slice(0, 4) + ' ' + tel.slice(4)}</a>
+export function Tel(tel: string) {
+  let full_tel = to_full_mobile_phone(tel)
+  if (!full_tel) return null
+  let formatted_tel = format_mobile_phone(full_tel)
+  return <a href={'tel:' + full_tel}>{formatted_tel}</a>
 }
 
 export function formatTel(tel: string | null): string | null {
   if (!tel) return null
-  tel = tel.replace('+852', '')
-  return tel.slice(0, 4) + ' ' + tel.slice(4)
+  return format_mobile_phone(tel)
 }
