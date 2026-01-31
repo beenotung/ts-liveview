a4a4d44adef92fd2b0a87c402fe5533af36846da
Beeno Tung <aabbcc1241@yahoo.com.hk>
2026-01-31 13:32:50 +0800

    seed: add sampel data in seed.ts

--- db/seed.ts
+++ db/seed.ts
@@ -5,3 +5,7 @@ import { proxy } from './proxy'
 //
 // You can setup the database with initial config and sample data via the db proxy.
 
+seedRow(proxy.task, { title: 'Try ts-liveview' }, { completed: true })
+seedRow(proxy.task, { title: 'Edit the home page' }, { completed: false })
+seedRow(proxy.task, { title: 'Create a new page' }, { completed: false })
+seedRow(proxy.task, { title: 'Share your thoughts' }, { completed: false })
