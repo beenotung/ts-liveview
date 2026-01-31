69170168219f655b9c9c9371975a03c58d9e955c
Beeno Tung <aabbcc1241@yahoo.com.hk>
2026-01-31 13:32:50 +0800

    seed: add sample data in seed.ts

--- db/seed.ts
+++ db/seed.ts
@@ -4,3 +4,8 @@ import { proxy } from './proxy'
 // This file serve like the knex seed file.
 //
 // You can setup the database with initial config and sample data via the db proxy.
+
+seedRow(proxy.task, { title: 'Try ts-liveview' }, { completed: true })
+seedRow(proxy.task, { title: 'Edit the home page' }, { completed: false })
+seedRow(proxy.task, { title: 'Create a new page' }, { completed: false })
+seedRow(proxy.task, { title: 'Share your thoughts' }, { completed: false })
