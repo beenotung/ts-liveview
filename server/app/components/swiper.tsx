6db8e183e5b9c02436986f95c329d08362391770
Beeno Tung <aabbcc1241@yahoo.com.hk>
2026-03-01 06:46:12 +0800

    chore: update swiper to avoid vulnerability message - not actually affected, since we're using custom js

--- server/app/components/swiper.tsx
+++ server/app/components/swiper.tsx
@@ -115,14 +115,36 @@ export function Swiper(
         ) : null}
         {attrs.showArrow ? (
           <>
-            <div
-              class="swiper-button-prev"
-              onclick="swiperSlide(this, -1)"
-            ></div>
-            <div
-              class="swiper-button-next"
-              onclick="swiperSlide(this, +1)"
-            ></div>
+            <div class="swiper-button-prev" onclick="swiperSlide(this, -1)">
+              <svg
+                class="swiper-navigation-icon"
+                width="11"
+                height="20"
+                viewBox="0 0 11 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M0.38296 20.0762C0.111788 19.805 0.111788 19.3654 0.38296 19.0942L9.19758 10.2796L0.38296 1.46497C0.111788 1.19379 0.111788 0.754138 0.38296 0.482966C0.654131 0.211794 1.09379 0.211794 1.36496 0.482966L10.4341 9.55214C10.8359 9.9539 10.8359 10.6053 10.4341 11.007L1.36496 20.0762C1.09379 20.3474 0.654131 20.3474 0.38296 20.0762Z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+            </div>
+            <div class="swiper-button-next" onclick="swiperSlide(this, +1)">
+              <svg
+                class="swiper-navigation-icon"
+                width="11"
+                height="20"
+                viewBox="0 0 11 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M0.38296 20.0762C0.111788 19.805 0.111788 19.3654 0.38296 19.0942L9.19758 10.2796L0.38296 1.46497C0.111788 1.19379 0.111788 0.754138 0.38296 0.482966C0.654131 0.211794 1.09379 0.211794 1.36496 0.482966L10.4341 9.55214C10.8359 9.9539 10.8359 10.6053 10.4341 11.007L1.36496 20.0762C1.09379 20.3474 0.654131 20.3474 0.38296 20.0762Z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+            </div>
           </>
         ) : null}
       </div>
