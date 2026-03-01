09d0ad3b05f594ff84815d4d43a2cd51a0d78aad
Beeno Tung <aabbcc1241@yahoo.com.hk>
2026-03-01 20:46:21 +0800

    chore: update generated file header

--- db/proxy.ts
+++ db/proxy.ts
@@ -1,3 +1,9 @@
+/**
+ * This file is auto generated, do not edit it manually.
+ *
+ * update command: npm run update
+ */
+
 import { proxySchema } from 'better-sqlite3-proxy'
 import { db } from './db'
 
