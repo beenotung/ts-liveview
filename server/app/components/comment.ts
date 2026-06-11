bd510d715e4dd8bcaa2cdfee6d093b7be31d408e
Beeno Tung <aabbcc1241@yahoo.com.hk>
2026-06-11 18:51:51 +0800

    feat: support i18n Locale in Comment helper function

--- server/app/components/comment.ts
+++ server/app/components/comment.ts
@@ -1,7 +1,17 @@
 import type { Raw } from '../jsx/types'
+import type { Context } from '../context'
+import { Locale, LocaleVariants } from './locale.js'
 
 /** For HTML comment */
-export function Comment(text: string): Raw {
+export function Comment(text: string): Raw
+export function Comment(attrs: LocaleVariants<string>, context: Context): Raw
+export function Comment(
+  text: string | LocaleVariants<string>,
+  context?: Context,
+): Raw {
+  if (typeof text !== 'string') {
+    text = Locale(text, context!)
+  }
   return ['raw', `<!-- ${text} -->`]
 }
 
